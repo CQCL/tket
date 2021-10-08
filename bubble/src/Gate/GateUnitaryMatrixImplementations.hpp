e84ee098fb9d452561d0f1709eda89da1cbfa7c7
Alec Edgington <54802828+cqc-alec@users.noreply.github.com>
2021-10-08 17:06:08 +0100

    [infra] Move all published docs into `doc`. (#55)

--- bubble/src/Gate/GateUnitaryMatrixImplementations.hpp
+++ bubble/src/Gate/GateUnitaryMatrixImplementations.hpp
@@ -38,7 +38,7 @@ struct GateUnitaryMatrixImplementations {
   // so dense->sparse routines would be needed).
 
   // Roughly follows the order in
-  // https://cqcl.github.io/tket/pytket/doc/html/optype.html
+  // https://cqcl.github.io/tket/doc/pytket/api/optype.html
 
   // Note: the below functions do not check double parameter values,
   // the caller must do that.
