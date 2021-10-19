ee7367d09a5daf8b703b6409b4c0dd3b2844e179
Luca Mondada <72734770+lmondada@users.noreply.github.com>
2021-10-19 17:40:41 +0100

    [infra] Updated license in `pytket/setup.py` (#78)

--- pytket/setup.py
+++ pytket/setup.py
@@ -205,7 +205,6 @@ setup(
     python_requires=">=3.7",
     url="https://cqcl.github.io/pytket",
     description="Python module for interfacing with the CQC tket library of quantum software",
-    license="CQC Software Licence",
     packages=setuptools.find_packages(),
     install_requires=[
         "sympy ~=1.6",
@@ -230,7 +229,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "License :: Other/Proprietary License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX :: Linux",
         "Operating System :: Microsoft :: Windows",
