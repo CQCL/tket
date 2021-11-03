3f125339ed7d690b527bfce21f12b207304f9c59
Alec Edgington <alec.edgington@cambridgequantum.com>
2021-11-03 17:37:41 +0000

    Define (empty) CompleteGraph class deriving from AbstractGraph.

---
+++ tket/src/Graphs/CompleteGraph.hpp
@@ -0,0 +1,27 @@
+// Copyright 2019-2021 Cambridge Quantum Computing
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef _TKET_CompleteGraph_H
+#define _TKET_CompleteGraph_H
+
+#include "AbstractGraph.hpp"
+
+namespace tket::graphs {
+
+template <typename T>
+class CompleteGraph : public AbstractGraph<T> {};
+
+}  // namespace tket::graphs
+
+#endif  // _TKET_CompleteGraph_H
