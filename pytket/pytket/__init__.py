6129ff3909f09f294ffd95558a47e3129c8a6734
Roland Guichard <roland.guichard@cambridgequantum.com>
2021-12-07 15:31:05 +0000

    Black autoformatting.

--- pytket/pytket/__init__.py
+++ pytket/pytket/__init__.py
@@ -27,4 +27,4 @@ import pytket.telemetry
 from pytket._version import __version__
 
 __path__ = __import__("pkgutil").extend_path(__path__, __name__)  # type: ignore
-__version__ = '.'.join(__version__.split('.')[:3])
+__version__ = ".".join(__version__.split(".")[:3])
