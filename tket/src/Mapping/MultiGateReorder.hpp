eba680a7f1a6ebc936a6e71220f8a44d269f5287
yao-cqc <75305462+yao-cqc@users.noreply.github.com>
2022-01-18 15:31:42 +0000

    Feature/reorder multi qubit gates (#157)
    * Add token swapping stage, add test
    
    * Update compilation passes to use new routing
    
    * Add json serialization
    
    * Continue adding JSON serialisation for routing_config
    
    * Improve Json definitions
    
    * Update JSON Serialization and use of Barrier
    
    * Change from reference_wrapper to shared_ptr
    
    * Add JSON_DECL for std::vector<RoutingMethodPtr>
    
    * format routing_test
    
    * Fix up tests and binders for python
    
    * Uncoment measurement tests
    
    * rename method to merge_ancilla
    
    * debug proptest
    
    * Make add_qubit add qubit to unit_bimaps_ if not nullptr
    
    * Architectures -> Architecture
    
    * Install boost on MacOS.
    
    * comments to debug
    
    * update proptest to support ancillas properly
    
    * remove couts
    
    * format
    
    * Make Unitary dimensions match
    
    * add tket assert for comparison
    
    * Update test to check value
    
    * add_qubit -> add_ancilla
    
    * Remove kwargs formatting from argument
    
    * Rename Architecture Methods
    
    * rename architecture methods
    
    * Allow architecture mapping to take original edges, to calculate Node to size_t mapping
    
    * add get_square_grid_edges, to allow fixed tests independent of SquareGrid
    
    * use ArchitectureMapping and edges in most tests, instead of Architecture
    
    * trivial typos, comments, cmake update
    
    * add copyright notices, pragma once, remove semicolon typos
    
    * update binders for inheritance and docs
    
    * format
    
    * Remove NodeGraph
    
    * update formatting
    
    * Reorder CZ circuits
    
    * Revert "Reorder CZ circuits"
    
    This reverts commit 9d67720f2047fb6875c6c520f37fb28967b3752e.
    
    * Make two methods in MappingFrontier public
    
    * Add MultiGateReorderRoutingMethod
    
    * Remove unnecessary frontier advancement
    
    * Avoid copying the whole MappingFrontier
    
    * Remove the edge_in_frontier method
    
    * Add comment for vertex rewiring
    
    * Allow users to set search limits
    
    * Change default max depth/size limits to 10
    
    * Fix using incorrect port colours
    
    * Obtain unitid by traversing to frontier instead of inputs
    
    * Add test for MultiGateReorderRoutingMethod
    
    * Refactor condition checks and rewire
    
    * Implement check_method
    
    * Add test for routing with LexiRoute
    
    Co-authored-by: sjdilkes <silas.dilkes@cambridgequantum.com>
    Co-authored-by: Alec Edgington <alec.edgington@cambridgequantum.com>
    Co-authored-by: Zen Harper <zen.harper@cambridgequantum.com>

---
+++ tket/src/Mapping/MultiGateReorder.hpp
@@ -0,0 +1,70 @@
+#ifndef _TKET_MultiGateReorder_H_
+#define _TKET_MultiGateReorder_H_
+
+#include "Mapping/MappingFrontier.hpp"
+#include "Mapping/RoutingMethod.hpp"
+
+namespace tket {
+
+class MultiGateReorder {
+ public:
+  /**
+   * Class Constructor
+   * @param _architecture Architecture object added operations must respect
+   * @param _mapping_frontier Contains Circuit object to be modified
+   */
+  MultiGateReorder(
+      const ArchitecturePtr& _architecture,
+      std::shared_ptr<MappingFrontier>& _mapping_frontier);
+
+  /**
+   * Try to commute any multi-qubit gates to the quantum frontier
+   * @param max_depth Maximum number of layers of gates checked for commutation.
+   * @param max_size Maximum number of gates checked for commutation.
+   */
+  void solve(unsigned max_depth, unsigned max_size);
+
+ private:
+  // Architecture all new physical operations must respect
+  ArchitecturePtr architecture_;
+  std::shared_ptr<MappingFrontier> mapping_frontier_;
+  EdgeVec u_frontier_edges_;
+};
+
+class MultiGateReorderRoutingMethod : public RoutingMethod {
+ public:
+  /**
+   * Checking and Routing methods redefined using MultiGateReorder.
+   * @param _max_depth Maximum number of layers of gates checked for
+   * commutation.
+   * @param _max_size Maximum number of gates checked for commutation.
+   */
+  MultiGateReorderRoutingMethod(
+      unsigned _max_depth = 10, unsigned _max_size = 10);
+
+  /**
+   * @return true if method can route subcircuit, false if not
+   */
+  bool check_method(
+      const std::shared_ptr<MappingFrontier>& /*mapping_frontier*/,
+      const ArchitecturePtr& /*architecture*/) const;
+
+  /**
+   * @param mapping_frontier Contains boundary of routed/unrouted circuit for
+   * modifying
+   * @param architecture Architecture providing physical constraints
+   * @return Logical to Physical mapping at boundary due to modification.
+   *
+   */
+  unit_map_t routing_method(
+      std::shared_ptr<MappingFrontier>& mapping_frontier,
+      const ArchitecturePtr& architecture) const;
+
+ private:
+  unsigned max_depth_;
+  unsigned max_size_;
+};
+
+}  // namespace tket
+
+#endif
---
+++ tket/tests/test_MultiGateReorder.cpp
@@ -0,0 +1,360 @@
+#include <catch2/catch.hpp>
+
+#include "Mapping/LexiRoute.hpp"
+#include "Mapping/MappingManager.hpp"
+#include "Mapping/MultiGateReorder.hpp"
+#include "Predicates/Predicates.hpp"
+#include "Simulation/CircuitSimulator.hpp"
+#include "Simulation/ComparisonFunctions.hpp"
+
+namespace tket {
+SCENARIO("Reorder circuits") {
+  std::vector<Node> nodes = {
+      Node("test_node", 0), Node("test_node", 1), Node("test_node", 2),
+      Node("node_test", 3)};
+
+  // n0 -- n1 -- n2 -- n3
+  Architecture architecture(
+      {{nodes[0], nodes[1]}, {nodes[1], nodes[2]}, {nodes[2], nodes[3]}});
+  ArchitecturePtr shared_arc = std::make_shared<Architecture>(architecture);
+  GIVEN("Simple CZ circuit.") {
+    Circuit circ(4);
+    std::vector<Qubit> qubits = circ.all_qubits();
+    // Physically invalid operations
+    circ.add_op<UnitID>(OpType::CZ, {qubits[0], qubits[2]});
+    circ.add_op<UnitID>(OpType::CZ, {qubits[0], qubits[3]});
+    // Physically valid operations
+    circ.add_op<UnitID>(OpType::CZ, {qubits[0], qubits[1]});
+    circ.add_op<UnitID>(OpType::CZ, {qubits[2], qubits[3]});
+    std::map<UnitID, UnitID> rename_map = {
+        {qubits[0], nodes[0]},
+        {qubits[1], nodes[1]},
+        {qubits[2], nodes[2]},
+        {qubits[3], nodes[3]}};
+    circ.rename_units(rename_map);
+    Circuit circ_copy(circ);
+    std::shared_ptr<MappingFrontier> mf =
+        std::make_shared<MappingFrontier>(circ);
+    mf->advance_frontier_boundary(shared_arc);
+    MultiGateReorder mr(shared_arc, mf);
+    mr.solve(20, 20);
+    std::vector<Command> commands = circ.get_commands();
+    for (unsigned i = 0; i < 2; i++) {
+      std::vector<Node> nodes;
+      for (auto arg : commands[i].get_args()) {
+        nodes.push_back(Node(arg));
+      }
+      REQUIRE(shared_arc->valid_operation(nodes));
+    }
+    const auto u = tket_sim::get_unitary(circ);
+    const auto u1 = tket_sim::get_unitary(circ_copy);
+    REQUIRE(tket_sim::compare_statevectors_or_unitaries(
+        u, u1, tket_sim::MatrixEquivalence::EQUAL));
+  }
+
+  GIVEN("Simple CZ circuit 2.") {
+    Circuit circ(4);
+    std::vector<Qubit> qubits = circ.all_qubits();
+    // Physically valid operations
+    circ.add_op<UnitID>(OpType::CZ, {qubits[1], qubits[0]});
+    // Physically invalid operations
+    circ.add_op<UnitID>(OpType::CZ, {qubits[0], qubits[2]});
+    circ.add_op<UnitID>(OpType::CZ, {qubits[0], qubits[3]});
+    // Physically valid operations
+    circ.add_op<UnitID>(OpType::CZ, {qubits[0], qubits[1]});
+    circ.add_op<UnitID>(OpType::CZ, {qubits[2], qubits[3]});
+    // Physically invalid operations
+    circ.add_op<UnitID>(OpType::CZ, {qubits[3], qubits[0]});
+    // Physically valid operations
+    circ.add_op<UnitID>(OpType::CZ, {qubits[3], qubits[2]});
+    std::map<UnitID, UnitID> rename_map = {
+        {qubits[0], nodes[0]},
+        {qubits[1], nodes[1]},
+        {qubits[2], nodes[2]},
+        {qubits[3], nodes[3]}};
+    circ.rename_units(rename_map);
+    Circuit circ_copy(circ);
+    std::shared_ptr<MappingFrontier> mf =
+        std::make_shared<MappingFrontier>(circ);
+    mf->advance_frontier_boundary(shared_arc);
+    MultiGateReorder mr(shared_arc, mf);
+    mr.solve(20, 20);
+    std::vector<Command> commands = circ.get_commands();
+    for (unsigned i = 0; i < 4; i++) {
+      std::vector<Node> nodes;
+      for (auto arg : commands[i].get_args()) {
+        nodes.push_back(Node(arg));
+      }
+      REQUIRE(shared_arc->valid_operation(nodes));
+    }
+    const auto u = tket_sim::get_unitary(circ);
+    const auto u1 = tket_sim::get_unitary(circ_copy);
+    REQUIRE(tket_sim::compare_statevectors_or_unitaries(
+        u, u1, tket_sim::MatrixEquivalence::EQUAL));
+  }
+  GIVEN("Simple CZ circuit with single_qs.") {
+    Circuit circ(4, 1);
+    std::vector<Qubit> qubits = circ.all_qubits();
+    // Physically valid operations
+    circ.add_op<UnitID>(OpType::CZ, {qubits[1], qubits[0]});
+    // Physically invalid operations
+    circ.add_op<UnitID>(OpType::CZ, {qubits[0], qubits[2]});
+    circ.add_op<UnitID>(OpType::CZ, {qubits[0], qubits[3]});
+    // Physically valid operations
+    circ.add_op<UnitID>(OpType::Rz, 0.5, {qubits[0]});
+    circ.add_op<UnitID>(OpType::Rz, 0.5, {qubits[2]});
+    circ.add_op<UnitID>(OpType::Rz, 0.5, {qubits[3]});
+    circ.add_op<UnitID>(OpType::CZ, {qubits[0], qubits[1]});
+    circ.add_op<UnitID>(OpType::Measure, {qubits[2], Bit(0)});
+    circ.add_op<UnitID>(OpType::CZ, {qubits[2], qubits[3]});
+    // Physically invalid operations
+    circ.add_op<UnitID>(OpType::CZ, {qubits[3], qubits[0]});
+    // Physically valid operations
+    circ.add_op<UnitID>(OpType::H, {qubits[3]});
+    circ.add_op<UnitID>(OpType::CZ, {qubits[3], qubits[2]});
+    std::map<UnitID, UnitID> rename_map = {
+        {qubits[0], nodes[0]},
+        {qubits[1], nodes[1]},
+        {qubits[2], nodes[2]},
+        {qubits[3], nodes[3]}};
+    circ.rename_units(rename_map);
+    Circuit circ_copy(circ);
+
+    std::shared_ptr<MappingFrontier> mf =
+        std::make_shared<MappingFrontier>(circ);
+    mf->advance_frontier_boundary(shared_arc);
+    MultiGateReorder mr(shared_arc, mf);
+    mr.solve(20, 20);
+    std::vector<Command> commands = circ.get_commands();
+    for (unsigned i = 0; i < 2; i++) {
+      std::vector<Node> nodes;
+      for (auto arg : commands[i].get_args()) {
+        nodes.push_back(Node(arg));
+      }
+      REQUIRE(shared_arc->valid_operation(nodes));
+    }
+    const auto u = tket_sim::get_unitary(circ);
+    const auto u1 = tket_sim::get_unitary(circ_copy);
+    REQUIRE(tket_sim::compare_statevectors_or_unitaries(
+        u, u1, tket_sim::MatrixEquivalence::EQUAL));
+  }
+
+  GIVEN("Circuit with multi qubit gates.") {
+    Circuit circ(4, 1);
+    std::vector<Qubit> qubits = circ.all_qubits();
+    // Physically valid operations
+    circ.add_op<UnitID>(OpType::CZ, {qubits[1], qubits[0]});
+    // Physically invalid operations
+    circ.add_op<UnitID>(OpType::CZ, {qubits[0], qubits[2]});
+    // Physically valid operations
+    circ.add_op<UnitID>(OpType::CCX, {qubits[1], qubits[2], qubits[3]});
+    circ.add_op<UnitID>(OpType::Rx, 0.5, {qubits[3]});
+    circ.add_op<UnitID>(OpType::CX, {qubits[2], qubits[3]});
+    circ.add_op<UnitID>(OpType::Rz, 0.5, {qubits[0]});
+    circ.add_op<UnitID>(OpType::CRz, 0.5, {qubits[0], qubits[1]});
+    circ.add_op<UnitID>(OpType::ZZPhase, 0.2, {qubits[0], qubits[1]});
+    // Physically invalid operations
+    circ.add_op<UnitID>(OpType::CZ, {qubits[3], qubits[0]});
+    // Physically valid operations
+    circ.add_op<UnitID>(OpType::H, {qubits[3]});
+    circ.add_op<UnitID>(OpType::CZ, {qubits[3], qubits[2]});
+
+    std::map<UnitID, UnitID> rename_map = {
+        {qubits[0], nodes[0]},
+        {qubits[1], nodes[1]},
+        {qubits[2], nodes[2]},
+        {qubits[3], nodes[3]}};
+    circ.rename_units(rename_map);
+    Circuit circ_copy(circ);
+
+    std::shared_ptr<MappingFrontier> mf =
+        std::make_shared<MappingFrontier>(circ);
+    mf->advance_frontier_boundary(shared_arc);
+    MultiGateReorder mr(shared_arc, mf);
+    mr.solve(20, 20);
+    std::vector<Command> commands = circ.get_commands();
+    for (unsigned i = 0; i < 6; i++) {
+      std::vector<Node> nodes;
+      for (auto arg : commands[i].get_args()) {
+        nodes.push_back(Node(arg));
+      }
+      REQUIRE(shared_arc->valid_operation(nodes));
+    }
+    const auto u = tket_sim::get_unitary(circ);
+    const auto u1 = tket_sim::get_unitary(circ_copy);
+    REQUIRE(tket_sim::compare_statevectors_or_unitaries(
+        u, u1, tket_sim::MatrixEquivalence::EQUAL));
+  }
+}
+
+SCENARIO("Reorder circuits with limited search space") {
+  std::vector<Node> nodes = {
+      Node("test_node", 0), Node("test_node", 1), Node("test_node", 2),
+      Node("node_test", 3)};
+
+  // n0 -- n1 -- n2 -- n3
+  Architecture architecture(
+      {{nodes[0], nodes[1]}, {nodes[1], nodes[2]}, {nodes[2], nodes[3]}});
+  ArchitecturePtr shared_arc = std::make_shared<Architecture>(architecture);
+  GIVEN("Simple CZ circuit.") {
+    Circuit circ(4);
+    std::vector<Qubit> qubits = circ.all_qubits();
+    // Physically invalid operations
+    circ.add_op<UnitID>(OpType::CZ, {qubits[0], qubits[2]});
+    circ.add_op<UnitID>(OpType::CZ, {qubits[0], qubits[3]});
+    // Physically valid operations
+    circ.add_op<UnitID>(OpType::CZ, {qubits[0], qubits[1]});
+    circ.add_op<UnitID>(OpType::CZ, {qubits[2], qubits[1]});
+    std::map<UnitID, UnitID> rename_map = {
+        {qubits[0], nodes[0]},
+        {qubits[1], nodes[1]},
+        {qubits[2], nodes[2]},
+        {qubits[3], nodes[3]}};
+    circ.rename_units(rename_map);
+    Circuit circ_copy(circ);
+    std::shared_ptr<MappingFrontier> mf =
+        std::make_shared<MappingFrontier>(circ);
+    mf->advance_frontier_boundary(shared_arc);
+    MultiGateReorder mr(shared_arc, mf);
+    mr.solve(3, 3);
+    // Check only the first valid CZ get commuted to the front
+    std::vector<Command> commands = circ.get_commands();
+    REQUIRE(shared_arc->valid_operation(
+        {Node(commands[0].get_args()[0]), Node(commands[0].get_args()[1])}));
+    REQUIRE(!shared_arc->valid_operation(
+        {Node(commands[1].get_args()[0]), Node(commands[1].get_args()[1])}));
+    const auto u = tket_sim::get_unitary(circ);
+    const auto u1 = tket_sim::get_unitary(circ_copy);
+    REQUIRE(tket_sim::compare_statevectors_or_unitaries(
+        u, u1, tket_sim::MatrixEquivalence::EQUAL));
+  }
+}
+
+SCENARIO("Test MultiGateReorderRoutingMethod") {
+  std::vector<Node> nodes = {
+      Node("test_node", 0), Node("test_node", 1), Node("test_node", 2),
+      Node("node_test", 3)};
+
+  // n0 -- n1 -- n2 -- n3
+  Architecture architecture(
+      {{nodes[0], nodes[1]}, {nodes[1], nodes[2]}, {nodes[2], nodes[3]}});
+  ArchitecturePtr shared_arc = std::make_shared<Architecture>(architecture);
+  GIVEN("Simple CZ circuit.") {
+    Circuit circ(4);
+    std::vector<Qubit> qubits = circ.all_qubits();
+    // Physically valid operations
+    circ.add_op<UnitID>(OpType::CZ, {qubits[0], qubits[1]});
+    circ.add_op<UnitID>(OpType::CZ, {qubits[2], qubits[3]});
+    // Physically invalid operations
+    circ.add_op<UnitID>(OpType::CZ, {qubits[0], qubits[2]});
+    circ.add_op<UnitID>(OpType::CZ, {qubits[0], qubits[3]});
+    // Physically valid operations
+    circ.add_op<UnitID>(OpType::CZ, {qubits[0], qubits[1]});
+    circ.add_op<UnitID>(OpType::CZ, {qubits[2], qubits[3]});
+    circ.add_op<UnitID>(OpType::CZ, {qubits[2], qubits[3]});
+    std::map<UnitID, UnitID> rename_map = {
+        {qubits[0], nodes[0]},
+        {qubits[1], nodes[1]},
+        {qubits[2], nodes[2]},
+        {qubits[3], nodes[3]}};
+    circ.rename_units(rename_map);
+    Circuit circ_copy(circ);
+    std::shared_ptr<MappingFrontier> mf =
+        std::make_shared<MappingFrontier>(circ);
+    mf->advance_frontier_boundary(shared_arc);
+    MultiGateReorderRoutingMethod mrrm;
+    REQUIRE(mrrm.check_method(mf, shared_arc));
+
+    unit_map_t init_map = mrrm.routing_method(mf, shared_arc);
+    REQUIRE(init_map.size() == 0);
+    std::vector<Command> commands = circ.get_commands();
+    for (unsigned i = 0; i < 5; i++) {
+      std::vector<Node> nodes;
+      for (auto arg : commands[i].get_args()) {
+        nodes.push_back(Node(arg));
+      }
+      REQUIRE(shared_arc->valid_operation(nodes));
+    }
+    const auto u = tket_sim::get_unitary(circ);
+    const auto u1 = tket_sim::get_unitary(circ_copy);
+    REQUIRE(tket_sim::compare_statevectors_or_unitaries(
+        u, u1, tket_sim::MatrixEquivalence::EQUAL));
+
+    // Test with limits
+    Circuit circ2(circ_copy);
+
+    std::shared_ptr<MappingFrontier> mf2 =
+        std::make_shared<MappingFrontier>(circ2);
+    mf2->advance_frontier_boundary(shared_arc);
+    MultiGateReorderRoutingMethod mrrm2(4, 4);
+    REQUIRE(mrrm2.check_method(mf2, shared_arc));
+
+    unit_map_t init_map2 = mrrm2.routing_method(mf2, shared_arc);
+    REQUIRE(init_map2.size() == 0);
+    std::vector<Command> commands2 = circ2.get_commands();
+    for (unsigned i = 0; i < 4; i++) {
+      std::vector<Node> nodes;
+      for (auto arg : commands2[i].get_args()) {
+        nodes.push_back(Node(arg));
+      }
+      REQUIRE(shared_arc->valid_operation(nodes));
+    }
+    std::vector<Node> nodes;
+    for (auto arg : commands2[4].get_args()) {
+      nodes.push_back(Node(arg));
+    }
+    REQUIRE(!shared_arc->valid_operation(nodes));
+    const auto u2 = tket_sim::get_unitary(circ2);
+    REQUIRE(tket_sim::compare_statevectors_or_unitaries(
+        u2, u1, tket_sim::MatrixEquivalence::EQUAL));
+  }
+}
+
+SCENARIO("Test MappingManager with MultiGateReorderRoutingMethod") {
+  std::vector<Node> nodes = {
+      Node("test_node", 0), Node("test_node", 1), Node("test_node", 2),
+      Node("node_test", 3)};
+
+  // n0 -- n1 -- n2 -- n3
+  Architecture architecture(
+      {{nodes[0], nodes[1]}, {nodes[1], nodes[2]}, {nodes[2], nodes[3]}});
+  ArchitecturePtr shared_arc = std::make_shared<Architecture>(architecture);
+
+  GIVEN("Simple CZ, CX circuit.") {
+    Circuit circ(4);
+    std::vector<Qubit> qubits = circ.all_qubits();
+
+    // Physically invalid operations
+    circ.add_op<UnitID>(OpType::CX, {qubits[0], qubits[2]});
+    circ.add_op<UnitID>(OpType::CX, {qubits[1], qubits[3]});
+    // Physically valid operations
+    circ.add_op<UnitID>(OpType::CX, {qubits[1], qubits[2]});
+    circ.add_op<UnitID>(OpType::CZ, {qubits[0], qubits[1]});
+    std::map<UnitID, UnitID> rename_map = {
+        {qubits[0], nodes[0]},
+        {qubits[1], nodes[1]},
+        {qubits[2], nodes[2]},
+        {qubits[3], nodes[3]}};
+    circ.rename_units(rename_map);
+    std::shared_ptr<MappingFrontier> mf =
+        std::make_shared<MappingFrontier>(circ);
+    MappingManager mm(shared_arc);
+    // MultiGateReorderRoutingMethod should first commute the last two gates
+    // then only one swap is needed.
+    std::vector<RoutingMethodPtr> vrm = {
+        std::make_shared<MultiGateReorderRoutingMethod>(),
+        std::make_shared<LexiRouteRoutingMethod>(10)};
+    bool res = mm.route_circuit(circ, vrm);
+    PredicatePtr routed_correctly =
+        std::make_shared<ConnectivityPredicate>(architecture);
+    REQUIRE(routed_correctly->verify(circ));
+    REQUIRE(circ.count_gates(OpType::SWAP) == 1);
+    std::vector<Command> commands = circ.get_commands();
+    REQUIRE(commands.size() == 5);
+    Command swap_c = commands[2];
+    unit_vector_t uids = {nodes[1], nodes[2]};
+    REQUIRE(swap_c.get_args() == uids);
+    REQUIRE(*swap_c.get_op_ptr() == *get_op_ptr(OpType::SWAP));
+  }
+}
+}  // namespace tket
