fdb22faec31d27d454222d4257e9b25392fbb42c
melf <cqc@melf.de>
2022-02-03 14:44:14 +0000

    fix binder include

--- pytket/binders/mapping.cpp
+++ pytket/binders/mapping.cpp
@@ -2,7 +2,6 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
-#include "Mapping/AASRoute.hpp"
 #include "Mapping/LexiRoute.hpp"
 #include "Mapping/MappingManager.hpp"
 #include "Mapping/RoutingMethodCircuit.hpp"
