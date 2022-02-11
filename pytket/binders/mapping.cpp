7c092f2b99f41da88aa89d3c35b6432bae6784ef
Silas Dilkes <36165522+sjdilkes@users.noreply.github.com>
2022-02-11 15:32:20 +0000

    Update pytket/binders/mapping.cpp
    Co-authored-by: Alec Edgington <54802828+cqc-alec@users.noreply.github.com>

--- pytket/binders/mapping.cpp
+++ pytket/binders/mapping.cpp
@@ -49,7 +49,7 @@ PYBIND11_MODULE(mapping, m) {
           "that given a Circuit and Architecture object, returns a tuple "
           "containing a new modified circuit, the initial logical to physical "
           "qubit mapping of the modified circuit and the permutation of "
-          "'logical to physical qubit mapping given operations in the "
+          "logical to physical qubit mapping given operations in the "
           "modified circuit\n:param check_subcircuit: A function declaration "
           "that given a Circuit and Architecture object, returns a bool "
           "stating whether the given method can modify the "
