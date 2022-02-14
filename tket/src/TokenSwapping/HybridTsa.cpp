3366e223a61ba12328b03a68dcf5187d86faef7f
Zen Harper <zen.harper@cambridgequantum.com>
2022-02-14 16:39:24 +0000

    rename HybridTsa00 -> HybridTsa; move files out of include directory

--- tket/src/TokenSwapping/HybridTsa.cpp
+++ tket/src/TokenSwapping/HybridTsa.cpp
@@ -1,4 +1,4 @@
-// Copyright 2019-2021 Cambridge Quantum Computing
+// Copyright 2019-2022 Cambridge Quantum Computing
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "HybridTsa00.hpp"
+#include "HybridTsa.hpp"
 
 #include "TokenSwapping/DistanceFunctions.hpp"
 #include "Utils/Assert.hpp"
@@ -22,23 +22,16 @@ using std::vector;
 namespace tket {
 namespace tsa_internal {
 
-HybridTsa00::HybridTsa00() {
+HybridTsa::HybridTsa() {
   m_name = "HybridTSA_00";
   m_trivial_tsa.set(TrivialTSA::Options::BREAK_AFTER_PROGRESS);
 }
 
-// GCOVR_EXCL_START
-CyclesPartialTsa& HybridTsa00::get_cycles_tsa_for_testing() {
-  return m_cycles_tsa;
-}
 
-TrivialTSA& HybridTsa00::get_trivial_tsa_for_testing() { return m_trivial_tsa; }
-// GCOVR_EXCL_STOP
-
-void HybridTsa00::append_partial_solution(
+void HybridTsa::append_partial_solution(
     SwapList& swaps, VertexMapping& vertex_mapping,
     DistancesInterface& distances, NeighboursInterface& neighbours,
-    PathFinderInterface& path_finder) {
+    RiverFlowPathFinder& path_finder) {
   const auto initial_L = get_total_home_distances(vertex_mapping, distances);
   for (size_t counter = initial_L + 1; counter > 0; --counter) {
     const auto swaps_before = swaps.size();
