0e742a8f0d7887b7c78e4f85b6e2c56887cc3295
Zen Harper <zen.harper@cambridgequantum.com>
2022-02-14 16:21:33 +0000

    change copyright to 2022

--- tket/src/TokenSwapping/TableLookup/CanonicalRelabelling.cpp
+++ tket/src/TokenSwapping/TableLookup/CanonicalRelabelling.cpp
@@ -1,4 +1,4 @@
-// Copyright 2019-2021 Cambridge Quantum Computing
+// Copyright 2019-2022 Cambridge Quantum Computing
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -19,7 +19,6 @@
 
 #include "Utils/Assert.hpp"
 
-;
 using std::vector;
 
 namespace tket {
