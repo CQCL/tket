f24e76bb5973f13f9d5e4a0af4db5b2792c78572
sjdilkes <silas.dilkes@cambridgequantum.com>
2022-02-16 19:10:47 +0000

    change length of line

--- pytket/pytket/architecture/__init__.py
+++ pytket/pytket/architecture/__init__.py
@@ -12,6 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""The `architecture` module provides an API to interact with the ::py:class:`Architecture` class."""
+"""The `architecture` module provides an API to interact with the 
+    ::py:class:`Architecture` class."""
 
 from pytket._tket.architecture import *  # type: ignore
