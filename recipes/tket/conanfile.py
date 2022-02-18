9c3415337f4f16bb802334b0bd35b7035ee4bce5
Alec Edgington <alec.edgington@cambridgequantum.com>
2022-02-18 10:57:09 +0000

    Update to symengine 0.9.0.

--- recipes/tket/conanfile.py
+++ recipes/tket/conanfile.py
@@ -42,7 +42,7 @@ class TketConan(ConanFile):
     requires = (
         "boost/1.78.0",
         # symengine from remote: https://tket.jfrog.io/artifactory/api/conan/tket-conan
-        "symengine/0.8.1.1@tket/stable",
+        "symengine/0.9.0@tket/stable",
         "eigen/3.4.0",
         "spdlog/1.9.2",
         "nlohmann_json/3.10.4",
