9b2864b8256429979bfcd9c6ee4b153a0fc6a72a
Alec Edgington <alec.edgington@cambridgequantum.com>
2022-03-01 19:07:17 +0000

    Remove unused import.

--- pytket/pytket/config/pytket_config.py
+++ pytket/pytket/config/pytket_config.py
@@ -15,7 +15,6 @@
 from abc import ABC, abstractmethod
 from pathlib import Path
 from typing import Any, ClassVar, Dict, Optional, Type, TypeVar
-from uuid import UUID
 from dataclasses import asdict
 import json
 import os
