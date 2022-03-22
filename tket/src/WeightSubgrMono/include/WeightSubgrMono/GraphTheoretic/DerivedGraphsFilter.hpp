24ad6864e4d1f2fe78d7d92ae7243d08da94386a
Zen Harper <zen.harper@cambridgequantum.com>
2022-03-22 11:08:45 +0000

    remove unused var from doxygen comment

--- tket/src/WeightSubgrMono/include/WeightSubgrMono/GraphTheoretic/DerivedGraphsFilter.hpp
+++ tket/src/WeightSubgrMono/include/WeightSubgrMono/GraphTheoretic/DerivedGraphsFilter.hpp
@@ -36,8 +36,6 @@ class DerivedGraphsFilter {
   /** Does the individual PV->TV mapping appear to be compatible?
    * @param pv A pattern vertex
    * @param tv A target vertex
-   * @param fixed_data Contains necessary data (specifically, NeighboursData),
-   * since the information is calculated lazily.
    * @return True if the mapping appears to be possible, false if it is
    * DEFINITELY always impossible, regardless of the other assignments. Thus, if
    * false, TV can be deleted from Dom(PV) at every level.
