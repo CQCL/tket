eac3980d12900279dd5fa5047392dc9999d2df11
drzenharper <70650270+drzenharper@users.noreply.github.com>
2022-05-16 18:07:42 +0100

    Update tket/src/Circuit/include/Circuit/Boxes.hpp
    Co-authored-by: Alec Edgington <54802828+cqc-alec@users.noreply.github.com>

--- tket/src/Circuit/include/Circuit/Boxes.hpp
+++ tket/src/Circuit/include/Circuit/Boxes.hpp
@@ -113,8 +113,7 @@ Op_ptr set_box_id(BoxT &b, boost::uuids::uuid newid) {
 class CircBox : public Box {
  public:
   /**
-   * Construct from a given circuit. The circuit must be simple; see
-   * https://github.com/CQCL/tket/issues/205
+   * Construct from a given circuit. The circuit must be simple.
    */
   explicit CircBox(const Circuit &circ);
 
