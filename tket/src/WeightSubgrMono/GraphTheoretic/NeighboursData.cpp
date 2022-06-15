e905a11cc01f564cfbb25a7855212f17dfa7daec
Zen Harper <zen.harper@cambridgequantum.com>
2022-06-15 17:49:08 +0100

    remove NeighboursData::initialise function; replace auto with type

--- tket/src/WeightSubgrMono/GraphTheoretic/NeighboursData.cpp
+++ tket/src/WeightSubgrMono/GraphTheoretic/NeighboursData.cpp
@@ -24,12 +24,6 @@
 namespace tket {
 namespace WeightedSubgraphMonomorphism {
 
-NeighboursData::NeighboursData() : m_number_of_edges(0) {}
-
-NeighboursData::NeighboursData(const GraphEdgeWeights& edges_and_weights) {
-  initialise(edges_and_weights);
-}
-
 std::size_t NeighboursData::get_number_of_edges() const {
   return m_number_of_edges;
 }
@@ -37,9 +31,10 @@ std::size_t NeighboursData::get_number_of_edges() const {
 std::vector<VertexWSM> NeighboursData::get_neighbours_expensive(
     VertexWSM v) const {
   std::vector<VertexWSM> result;
-  const auto& neighbours_and_weights = get_neighbours_and_weights(v);
+  const std::vector<std::pair<VertexWSM, WeightWSM>>& neighbours_and_weights =
+      get_neighbours_and_weights(v);
   result.reserve(neighbours_and_weights.size());
-  for (const auto& entry : neighbours_and_weights) {
+  for (const std::pair<VertexWSM, WeightWSM>& entry : neighbours_and_weights) {
     result.push_back(entry.first);
   }
   return result;
@@ -49,13 +44,13 @@ std::size_t NeighboursData::get_number_of_nonisolated_vertices() const {
   return m_neighbours_and_weights.size();
 }
 
-void NeighboursData::initialise(const GraphEdgeWeights& edges_and_weights) {
+NeighboursData::NeighboursData(const GraphEdgeWeights& edges_and_weights) {
   // Only the edges (v1,v2) with v1<v2.
   std::set<std::pair<VertexWSM, VertexWSM>> ordered_edges_seen;
   std::set<VertexWSM> vertices_seen;
-  for (const auto& entry : edges_and_weights) {
-    const auto& v1 = entry.first.first;
-    const auto& v2 = entry.first.second;
+  for (const std::pair<EdgeWSM, WeightWSM>& entry : edges_and_weights) {
+    const VertexWSM& v1 = entry.first.first;
+    const VertexWSM& v2 = entry.first.second;
     vertices_seen.insert(v1);
     vertices_seen.insert(v2);
     if (v1 == v2) {
@@ -70,10 +65,11 @@ void NeighboursData::initialise(const GraphEdgeWeights& edges_and_weights) {
       *vertices_seen.crbegin() != vertices_seen.size() - 1) {
     throw std::runtime_error("Vertices should be [0,1,2,...,N].");
   }
-  for (const auto& edge : ordered_edges_seen) {
-    const auto weight_opt = get_optional_value(edges_and_weights, edge);
+  for (const EdgeWSM& edge : ordered_edges_seen) {
+    const std::optional<WeightWSM> weight_opt =
+        get_optional_value(edges_and_weights, edge);
     if (weight_opt) {
-      const auto other_weight_opt = get_optional_value(
+      const std::optional<WeightWSM> other_weight_opt = get_optional_value(
           edges_and_weights, std::make_pair(edge.second, edge.first));
       if (other_weight_opt && weight_opt.value() != other_weight_opt.value()) {
         throw std::runtime_error("Edge weights mismatch");
@@ -82,16 +78,15 @@ void NeighboursData::initialise(const GraphEdgeWeights& edges_and_weights) {
   }
   m_number_of_edges = ordered_edges_seen.size();
   m_neighbours_and_weights.resize(vertices_seen.size());
-  for (auto& entry : m_neighbours_and_weights) {
-    entry.clear();
-  }
-  for (const auto& edge : ordered_edges_seen) {
-    const auto& v1 = edge.first;
-    const auto& v2 = edge.second;
+
+  for (const EdgeWSM& edge : ordered_edges_seen) {
+    const VertexWSM& v1 = edge.first;
+    const VertexWSM& v2 = edge.second;
     TKET_ASSERT(v1 < v2);
     TKET_ASSERT(v2 < vertices_seen.size());
     WeightWSM weight;
-    const auto weight_opt = get_optional_value(edges_and_weights, edge);
+    const std::optional<WeightWSM> weight_opt =
+        get_optional_value(edges_and_weights, edge);
     if (weight_opt) {
       weight = weight_opt.value();
     } else {
@@ -100,7 +95,8 @@ void NeighboursData::initialise(const GraphEdgeWeights& edges_and_weights) {
     m_neighbours_and_weights[v1].emplace_back(v2, weight);
     m_neighbours_and_weights[v2].emplace_back(v1, weight);
   }
-  for (auto& neigh_data : m_neighbours_and_weights) {
+  for (std::vector<std::pair<VertexWSM, WeightWSM>>& neigh_data :
+       m_neighbours_and_weights) {
     // Automatically sort by vertex first; lexicographic.
     std::sort(neigh_data.begin(), neigh_data.end());
     TKET_ASSERT(is_sorted_and_unique(neigh_data));
@@ -112,7 +108,8 @@ std::optional<WeightWSM> NeighboursData::get_edge_weight_opt(
   if (v1 >= m_neighbours_and_weights.size()) {
     return {};
   }
-  const auto& v1_data = m_neighbours_and_weights[v1];
+  const std::vector<std::pair<VertexWSM, WeightWSM>>& v1_data =
+      m_neighbours_and_weights[v1];
   // (x,0) = (x,min) <= (x,w) <= (x+1, y) in lexicographic order
   std::pair<VertexWSM, WeightWSM> key;
   key.first = v2;
@@ -147,7 +144,8 @@ std::vector<std::size_t> NeighboursData::get_sorted_degree_sequence_expensive(
   if (v >= m_neighbours_and_weights.size()) {
     return result;
   }
-  for (const auto& entry : m_neighbours_and_weights[v]) {
+  for (const std::pair<VertexWSM, WeightWSM>& entry :
+       m_neighbours_and_weights[v]) {
     result.push_back(get_degree(entry.first));
   }
   std::sort(result.begin(), result.end());
@@ -169,7 +167,8 @@ std::vector<WeightWSM> NeighboursData::get_weights_expensive() const {
     // Every edge is implicitly stored twice, for (v1, v2) and (v2, v1).
     // To avoid duplicates, only write the weight when v1>v2.
     // The neighbour edges are stored with increasing v, as always.
-    for (const auto& inner_entry : m_neighbours_and_weights[v1]) {
+    for (const std::pair<VertexWSM, WeightWSM>& inner_entry :
+         m_neighbours_and_weights[v1]) {
       const VertexWSM& v2 = inner_entry.first;
       if (v2 > v1) {
         break;
