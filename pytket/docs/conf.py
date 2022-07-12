1e74b57d1e12b15df11ed4869e905e266e025edf
Alec Edgington <alec.edgington@cambridgequantum.com>
2022-07-12 09:14:50 +0100

    Update version number and changelog in docs.

--- pytket/docs/conf.py
+++ pytket/docs/conf.py
@@ -38,9 +38,9 @@ copyright = "2022, Cambridge Quantum Computing Ltd"
 author = "Cambridge Quantum Computing Ltd"
 
 # The short X.Y version
-version = "1.3"
+version = "1.4"
 # The full version, including alpha/beta/rc tags
-release = "1.3.0"
+release = "1.4.0"
 
 
 # -- General configuration ---------------------------------------------------
