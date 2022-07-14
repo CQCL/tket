d5bae765da89e37cfd71ac0aa68be8bd20bcd732
Alec Edgington <alec.edgington@cambridgequantum.com>
2022-07-14 12:36:54 +0100

    Add TODO comment.

--- tket/src/Transformations/ThreeQubitSquash.cpp
+++ tket/src/Transformations/ThreeQubitSquash.cpp
@@ -112,6 +112,7 @@ static Circuit candidate_sub(const Circuit &circ, OpType target_2qb_gate) {
   unsigned n_qb = circ.n_qubits();
   if (n_qb == 2) {
     Circuit repl = two_qubit_canonical(get_matrix_from_2qb_circ(circ));
+    // TODO Remove this once `clifford_simp` supports TK2.
     if (target_2qb_gate == OpType::CX) {
       clifford_simp(false).apply(repl);
     }
