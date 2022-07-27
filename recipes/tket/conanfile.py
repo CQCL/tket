1c7a642927cdc51725e0d6d0f201fa723e96a7ca
Alec Edgington <54802828+cqc-alec@users.noreply.github.com>
2022-07-27 08:33:43 +0100

    [infra] Switch to quantinuum conan repo (#457)

--- recipes/tket/conanfile.py
+++ recipes/tket/conanfile.py
@@ -41,11 +41,11 @@ class TketConan(ConanFile):
         "symengine/0.9.0",
         "eigen/3.4.0",
         "nlohmann_json/3.10.5",
-        "tklog/0.1.1@tket/stable",
-        "tkassert/0.1.0@tket/stable",
-        "tkrng/0.1.1@tket/stable",
-        "tktokenswap/0.1.0@tket/stable",
-        "tkwsm/0.1.0@tket/stable",
+        "tklog/0.1.2@tket/stable",
+        "tkassert/0.1.1@tket/stable",
+        "tkrng/0.1.2@tket/stable",
+        "tktokenswap/0.1.1@tket/stable",
+        "tkwsm/0.1.1@tket/stable",
     )
 
     # List of components in a topological sort according to dependencies:
