b9bfb7a9186d9859059b21f74a84ed7679371e33
sjdilkes <silas.dilkes@cambridgequantum.com>
2022-09-07 16:29:20 +0100

    Refactored Placement.hpp and Placement.cpp

--- tket/src/Placement/Placement.cpp
+++ tket/src/Placement/Placement.cpp
@@ -12,64 +12,142 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "Placement.hpp"
+#include "Placement/Placement.hpp"
 
+#include "Utils/HelperFunctions.hpp"
 
 namespace tket {
 
 bool Placement::place(Circuit& circ_) const {
-    std::map<UnitID, UnitID> map_ = this->get_placement_map(circ_);
-    std::map<Qubit, Node> recast_map;
-    for(const std::pair<UnitID, UnitID>& entry : map_){
-        recast_map.insert({Qubit(entry.first), Node(entry.second)});
-    }
-    return circ_.rename_units(recast_map);
+  std::map<UnitID, UnitID> map_ = this->get_placement_map(circ_);
+  std::map<Qubit, Node> recast_map;
+  for (const auto& entry : map_) {
+    recast_map.insert({Qubit(entry.first), Node(entry.second)});
+  }
+  return circ_.rename_units(recast_map);
 }
 
-std::map<UnitID, UnitID> Placement::get_placement_map(const Circuit& circ_) const{
-    std::vector<std::map<UnitID, UnitID>> all_maps = this->get_all_placement_maps(circ_);
-    // basic handling to avoid segmentation faults, as placement method may not
-    // return any valid map
-    auto it = all_maps.begin();
-    if(it != all_maps.end()){
-        return *it;
-    }
-    else{
-        return {};
-    }
-}   
-
-std::vector<std::map<UnitID, UnitID>> Placement::get_all_placement_maps(const Circuit& circ_) const {
+std::map<UnitID, UnitID> Placement::get_placement_map(
+    const Circuit& circ_) const {
+  std::vector<std::map<UnitID, UnitID>> all_maps =
+      this->get_all_placement_maps(circ_);
+  // basic handling to avoid segmentation faults, as placement method may not
+  // return any valid map
+  auto it = all_maps.begin();
+  if (it != all_maps.end()) {
+    return *it;
+  } else {
     return {};
+  }
 }
 
-QubitGraph::UndirectedConnGraph GraphPlacement::construct_pattern_graph(const std::vector<WeightedEdge>& edges) const {
-    QubitGraph q_graph();
-    for(const WeightedEdge& weighted_edge : edges){
-        Node node0 = Node(weighted_edge.node0);
-        Node node1 = Node(weighted_edge.node1);
-        bool e_01_exists = q_graph.edge_exists(node0, node1);
-        bool e_10_exists = q_graph.edge_exists(node1, node0);
-        if(e_01_exists || e_10_exists){
-            throw std::invalid_argument("Graph can only have on edge between a pair of Node.");
+std::vector<std::map<UnitID, UnitID>> Placement::get_all_placement_maps(
+    const Circuit& /*circ_*/) const {
+  return {};
+}
+
+const std::vector<GraphPlacement::WeightedEdge>
+GraphPlacement::default_weighting(const Circuit& circuit) {
+  Placement::Frontier frontier(circuit);
+  unsigned max_gates = 100, max_depth = 100, gate_counter = 0;
+  std::vector<GraphPlacement::WeightedEdge> weights;
+  for (unsigned i = 0;
+       i < max_depth && gate_counter < max_gates && !frontier.slice->empty();
+       i++) {
+    for (const Vertex& vert : *frontier.slice) {
+      EdgeVec q_out_edges =
+          circuit.get_out_edges_of_type(vert, EdgeType::Quantum);
+      unsigned n_q_edges = q_out_edges.size();
+      if (n_q_edges == 2) {
+        UnitID uid_0, uid_1;
+        Edge edge_0 = q_out_edges[0];
+        Edge edge_1 = q_out_edges[1];
+        bool match_0 = false, match_1 = false;
+        for (const std::pair<UnitID, Edge>& pair :
+             frontier.quantum_out_edges->get<TagKey>()) {
+          if (!match_0 && pair.second == edge_0) {
+            uid_0 = pair.first;
+          }
+          if (!match_1 && pair.second == edge_1) {
+            uid_1 = pair.first;
+          }
+          if (match_0 && match_1) {
+            break;
+          }
         }
-        if(!node_exists(node0)){
-            q_graph.add_node(node0);
+
+        bool match_weight = false;
+        for (WeightedEdge& weighted_edge : weights) {
+          //   std::pair<UnitID, UnitID> uids = uid_w.first;
+          if ((weighted_edge.node0 == uid_0 && weighted_edge.node1 == uid_1) ||
+              (weighted_edge.node0 == uid_1 && weighted_edge.node1 == uid_0)) {
+            // actually update the weight here, i.e. this is the "magic"
+            match_weight = true;
+            weighted_edge.weight += (max_depth - i);
+            break;
+          }
         }
-        if(!node_exists(node1)){
-            q_graph.add_node(node1);
+        if (!match_weight) {
+          weights.push_back({uid_0, uid_1, max_depth - i});
         }
-        q_graph.add_connection(node0, node1, weighted_edge.weight);
+        gate_counter++;
+      }
+      if (n_q_edges > 2) {
+        throw std::invalid_argument(
+            "Can only weight for Circuits with maximum two qubit gates.");
+      }
     }
-    return q_graph;
+  }
+  return weights;
 }
 
-
-std::vector<std::map<UnitID, UnitID>> GraphPlacement::get_all_placement_maps(const Circuit& circ_) const {
-    std::vector<WeightedEdge> weighted_edges = this->weight_pattern_graph_(circ_);
-    QubitGraph::UndirectedConnGraph pattern_graph = this->construct_pattern_graph(weighted_edges);
-    Architecture::UndirectedConnGraph target_graph = this->arc_.get_undirected_connectivity();
-    std::vector<qubit_bimap_t> all_maps = get_weighted_subgraph_monomorphism(pattern_graph, target_graph, this->maximum_matches_, this->timeout_);
-    return all_maps;
-}    
+QubitGraph GraphPlacement::construct_pattern_graph(
+    const std::vector<WeightedEdge>& edges) const {
+  QubitGraph q_graph;
+  for (const WeightedEdge& weighted_edge : edges) {
+    Node node0 = Node(weighted_edge.node0);
+    Node node1 = Node(weighted_edge.node1);
+    bool e_01_exists = q_graph.edge_exists(node0, node1);
+    bool e_10_exists = q_graph.edge_exists(node1, node0);
+    if (e_01_exists || e_10_exists) {
+      throw std::invalid_argument(
+          "Graph can only have on edge between a pair of Node.");
+    }
+    if (!q_graph.node_exists(node0)) {
+      q_graph.add_node(node0);
+    }
+    if (!q_graph.node_exists(node1)) {
+      q_graph.add_node(node1);
+    }
+    if (weighted_edge.weight > 0) {
+      q_graph.add_connection(node0, node1, weighted_edge.weight);
+    }
+  }
+  return q_graph;
 }
+
+std::vector<std::map<UnitID, UnitID>> GraphPlacement::get_all_placement_maps(
+    const Circuit& circ_) const {
+  std::vector<WeightedEdge> weighted_edges = this->weight_pattern_graph_(circ_);
+  QubitGraph pattern_graph = this->construct_pattern_graph(weighted_edges);
+  Architecture::UndirectedConnGraph target_graph =
+      this->arc_.get_undirected_connectivity();
+  std::vector<boost::bimap<Qubit, Node>> all_bimaps =
+      this->get_weighted_subgraph_monomorphisms(
+          pattern_graph, target_graph, this->maximum_matches_, this->timeout_);
+  std::vector<std::map<Qubit, Node>> all_qmaps;
+  for (boost::bimap<Qubit, Node>& bm : all_bimaps) {
+    all_qmaps.push_back(bimap_to_map(bm.left));
+  }
+  //   TODO: Update this to convert to UnitID to start with, this is
+  //   unncessary...
+  std::vector<std::map<UnitID, UnitID>> all_uidmap;
+  for (const auto& map : all_qmaps) {
+    std::map<UnitID, UnitID> uid_map;
+    for (const auto& q_n : map) {
+      uid_map.insert({UnitID(q_n.first), UnitID(q_n.second)});
+    }
+  }
+  return all_uidmap;
+}
+}  // namespace tket
--- tket/src/Placement/include/Placement/Placement.hpp
+++ tket/src/Placement/include/Placement/Placement.hpp
@@ -15,103 +15,143 @@
 #pragma once
 
 #include "Architecture/Architecture.hpp"
+#include "Circuit/Circuit.hpp"
+#include "Graphs/QubitGraph.hpp"
 
 namespace tket {
 
 class Placement {
- public: 
+ public:
+  typedef std::shared_ptr<Placement> Ptr;
 
-  typedef std:::shared_ptr<Placement> Ptr;
+  struct Frontier {
+    // set of 2qb vertices which need to be solved for
+    std::shared_ptr<Slice> slice;
+    // Quantum Edges coming in to vertices in slice, indexed by qubit
+    std::shared_ptr<unit_frontier_t> quantum_in_edges;
+    // Quantum Edges leaving vertices in slice, indexed by qubit
+    std::shared_ptr<unit_frontier_t> quantum_out_edges;
+    // Boolean edges coming in to vertices in slice. Guarantees that all edges
+    // into every vertex in slice is represented in next_cut
+    std::shared_ptr<b_frontier_t> boolean_in_edges;
 
-    explicit Placement(const Architecture& _arc) : arc_(_arc) {}
-    
-    /**
-     * Reassigns some UnitID in circ_ as UnitID arc_
-     * 
-     * @param circ_ Circuit to be relabelled
-     * 
-     */
-    bool place(Circuit& circ_) const;
+    // reference to circuit that it acts on
+    const Circuit& circ;
 
-    /**
-     * 
-     * For some Circuit, returns a map between Circuit UnitID and
-     * Architecture UnitID that can be used for reassigning UnitID in
-     * Circuit. Map is expected to give best performance for given method.
-     * 
-     * @param circ_ Circuit relabelling map is constructed from
-     * 
-     * @return Map between Circuit and Architecture UnitID
-     */
-    std::map<UnitID, UnitID> get_placement_map(const Circuit& circ_) const;
+    // initialise at front of circuit
+    explicit Frontier(const Circuit& _circ);
+    // move to next slice
+    void next_slicefrontier();
+  };
 
+  /**
+   *
+   */
+  explicit Placement(const Architecture& _arc) : arc_(_arc) {}
 
-    /**
-     * 
-     * For some Circuit, returns maps between Circuit UnitID and
-     * Architecture UnitID that can be used for reassigning UnitID in
-     * Circuit. Maps expected to give similiar performance for given method.
-     * 
-     * @param circ_ Circuit relabelling map is constructed from
-     * 
-     * @return Map between Circuit and Architecture UnitID
-     */
-    virtual std::vector<std::map<UnitID, UnitID>> get_all_placement_maps(
+  /**
+   *
+   */
+  Placement(){};
+
+  /**
+   * Reassigns some UnitID in circ_ as UnitID arc_
+   *
+   * @param circ_ Circuit to be relabelled
+   *
+   */
+  bool place(Circuit& circ_) const;
+
+  /**
+   *
+   * For some Circuit, returns a map between Circuit UnitID and
+   * Architecture UnitID that can be used for reassigning UnitID in
+   * Circuit. Map is expected to give best performance for given method.
+   *
+   * @param circ_ Circuit relabelling map is constructed from
+   *
+   * @return Map between Circuit and Architecture UnitID
+   */
+  std::map<UnitID, UnitID> get_placement_map(const Circuit& circ_) const;
+
+  /**
+   *
+   * For some Circuit, returns maps between Circuit UnitID and
+   * Architecture UnitID that can be used for reassigning UnitID in
+   * Circuit. Maps expected to give similiar performance for given method.
+   *
+   * @param circ_ Circuit relabelling map is constructed from
+   *
+   * @return Map between Circuit and Architecture UnitID
+   */
+  virtual std::vector<std::map<UnitID, UnitID>> get_all_placement_maps(
       const Circuit& circ_) const;
 
  protected:
-    Architecture arc_;
-
+  Architecture arc_;
 };
 
 JSON_DECL(Placement::Ptr);
 
-
-
-class GraphPlacement {
+class GraphPlacement : public Placement {
  public:
-    /**
-     * Holds information for constructing a weighted edge in a QubitGraph.
-     * @param node0 UnitID for first node in edge
-     * @param node1 UnitID for second node in edge
-     * @param weight Unsigned giving a weight for implied edge
-     */
-    struct WeightedEdge {
-        UnitID node0;
-        UnitID node1;
-        unsigned weight;
-    };
+  /**
+   * Holds information for constructing a weighted edge in a QubitGraph.
+   * @param node0 UnitID for first node in edge
+   * @param node1 UnitID for second node in edge
+   * @param weight Unsigned giving a weight for implied edge
+   */
+  struct WeightedEdge {
+    UnitID node0;
+    UnitID node1;
+    unsigned weight;
+  };
 
-    static const std::vector<WeightedEdge> default_weighting(const Circuit& circuit);
+  static const std::vector<WeightedEdge> default_weighting(
+      const Circuit& circuit);
 
-    explicit GraphPlacement(const Architecture& _arc, unsigned _maximum_matches, unsigned _timeout, const std::function<std::vector<WeightedEdge>(const Circuit&)> _weight_pattern_graph) : arc_(_arc), maximum_matches_(_maximum_matches), timeout_(_timeout), weight_pattern_graph_(_weight_pattern_graph) {};
+  explicit GraphPlacement(
+      const Architecture& _arc, unsigned _maximum_matches, unsigned _timeout,
+      const std::function<std::vector<WeightedEdge>(const Circuit&)>
+          _weight_pattern_graph = default_weighting)
+      : weight_pattern_graph_(_weight_pattern_graph),
+        maximum_matches_(_maximum_matches),
+        timeout_(_timeout) {
+    arc_ = _arc;
+  }
 
-    /**
-     * 
-     * For some Circuit, returns maps between Circuit UnitID and
-     * Architecture UnitID that can be used for reassigning UnitID in
-     * Circuit. Maps are constructed by running a Weighted Subgraph Monomorphism
-     * for the given problem and returning maximum_matches_ number of
-     * potential solutions, ranked.
-     * 
-     * @param circ_ Circuit relabelling map is constructed from
-     * 
-     * @return Map between Circuit and Architecture UnitID
-     */
-    std::vector<std::map<UnitID, UnitID>> get_all_placement_maps(
+  /**
+   *
+   * For some Circuit, returns maps between Circuit UnitID and
+   * Architecture UnitID that can be used for reassigning UnitID in
+   * Circuit. Maps are constructed by running a Weighted Subgraph Monomorphism
+   * for the given problem and returning maximum_matches_ number of
+   * potential solutions, ranked.
+   *
+   * @param circ_ Circuit relabelling map is constructed from
+   *
+   * @return Map between Circuit and Architecture UnitID
+   */
+  std::vector<std::map<UnitID, UnitID>> get_all_placement_maps(
       const Circuit& circ_) const override;
 
-
-
  protected:
+  const std::function<std::vector<WeightedEdge>(const Circuit&)>
+      weight_pattern_graph_;
+  unsigned maximum_matches_;
+  unsigned timeout_;
 
-    const std::function<std::vector<WeightedEdge>(const Circuit&)> weight_pattern_graph_;
-    unsigned maximum_matches_;
-    unsigned timeout_;
+  QubitGraph construct_pattern_graph(
+      const std::vector<WeightedEdge>& edges) const;
 
-    QubitGraph::UndirectedConnGraph construct_pattern_graph(const std::vector<WeightedEdge>& edges) const;
-
+  /** Solves the pure unweighted subgraph monomorphism problem, trying
+   * to embed the pattern graph into the target graph.
+   * Note that graph edge weights are IGNORED by this function.
+   */
+  std::vector<boost::bimap<Qubit, Node>> get_weighted_subgraph_monomorphisms(
+      const QubitGraph& pattern_graph,
+      const Architecture::UndirectedConnGraph& target_graph,
+      unsigned max_matches, unsigned timeout_ms) const;
 };
 
-
-} // namespace tket
+}  // namespace tket
