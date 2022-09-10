0d5022c8e9be7265bb5e1c7a07d18e02fbb94844
Alec Edgington <54802828+cqc-alec@users.noreply.github.com>
2022-09-10 06:12:41 +0100

    [infra] Upload tket conan packages (#512)

--- recipes/tket/conanfile.py
+++ recipes/tket/conanfile.py
@@ -20,7 +20,7 @@ import shutil
 
 class TketConan(ConanFile):
     name = "tket"
-    version = "1.0.1"
+    version = "1.0.2"
     license = "CQC Proprietary"
     homepage = "https://github.com/CQCL/tket"
     url = "https://github.com/conan-io/conan-center-index"
@@ -36,7 +36,7 @@ class TketConan(ConanFile):
     exports_sources = ["../../tket/src/*", "!*/build/*"]
     requires = (
         # tk* libraries may come from remote:
-        # https://tket.jfrog.io/artifactory/api/conan/tket-conan
+        # https://quantinuumsw.jfrog.io/artifactory/api/conan/tket1-libs
         "boost/1.79.0",
         "symengine/0.9.0",
         "eigen/3.4.0",
