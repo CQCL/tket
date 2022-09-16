a6439db0a0096fd6cc5d79899ff44b59c3832a30
Alec Edgington <54802828+cqc-alec@users.noreply.github.com>
2022-09-16 12:10:01 +0100

    [infra] Update versions of boost, tktokenswap and tkwsm. (#538)

--- recipes/tket-proptests/conanfile.py
+++ recipes/tket-proptests/conanfile.py
@@ -28,7 +28,7 @@ class TketProptestsConan(ConanFile):
     generators = "cmake"
     exports_sources = "../../tket/proptests/*"
     requires = (
-        "tket/1.0.5@tket/stable",
+        "tket/1.0.6@tket/stable",
         "rapidcheck/cci.20220514",
     )
 
