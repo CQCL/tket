d49ae81d761b29039863e17c59ed580022801b74
Alec Edgington <54802828+cqc-alec@users.noreply.github.com>
2022-09-21 17:32:48 +0100

    [infra] Update nlohmann_json to 3.11.2. (#468)

--- recipes/tket/conanfile.py
+++ recipes/tket/conanfile.py
@@ -20,7 +20,7 @@ import shutil
 
 class TketConan(ConanFile):
     name = "tket"
-    version = "1.0.10"
+    version = "1.0.11"
     license = "CQC Proprietary"
     homepage = "https://github.com/CQCL/tket"
     url = "https://github.com/conan-io/conan-center-index"
@@ -40,7 +40,7 @@ class TketConan(ConanFile):
         "boost/1.80.0",
         "symengine/0.9.0@tket/stable",
         "eigen/3.4.0",
-        "nlohmann_json/3.10.5",
+        "nlohmann_json/3.11.2",
         "tklog/0.1.2@tket/stable",
         "tkassert/0.1.1@tket/stable",
         "tkrng/0.1.2@tket/stable",
