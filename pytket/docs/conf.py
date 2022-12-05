0e89be05cd1af4b258b49e8f541c031337f65b0d
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2022-12-05 10:58:38 +0000

    change to sphinx-book-theme

--- pytket/docs/conf.py
+++ pytket/docs/conf.py
@@ -92,7 +92,7 @@ pygments_style = "sphinx"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "sphinx-book-theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -203,10 +203,10 @@ autodoc_member_order = "groupwise"
 
 # The following code is for resolving broken hyperlinks in the doc.
 
-from typing import Any, Dict, List
-from sphinx.application import Sphinx
 import re
+from typing import Any, Dict, List
 
+from sphinx.application import Sphinx
 
 # "matching name" : "correct name"
 custom_internal_mapping = {
