fd7dbea7d20f93b3d74d07400a6fee5f417fbbac
Alec Edgington <alec.edgington@cambridgequantum.com>
2022-12-13 09:59:49 +0000

    Bump tket version.

--- recipes/tket/conanfile.py
+++ recipes/tket/conanfile.py
@@ -20,7 +20,7 @@ import shutil
 
 class TketConan(ConanFile):
     name = "tket"
-    version = "1.0.34"
+    version = "1.0.35"
     license = "CQC Proprietary"
     homepage = "https://github.com/CQCL/tket"
     url = "https://github.com/conan-io/conan-center-index"
