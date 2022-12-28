31492b57270d9f7a4cb624f4e696f4cb046eb549
Yao Tang <yao.tang@cambridgequantum.com>
2022-12-28 13:58:53 +0000

    Update tket version

--- recipes/tket/conanfile.py
+++ recipes/tket/conanfile.py
@@ -20,7 +20,7 @@ import shutil
 
 class TketConan(ConanFile):
     name = "tket"
-    version = "1.0.36"
+    version = "1.0.37"
     license = "CQC Proprietary"
     homepage = "https://github.com/CQCL/tket"
     url = "https://github.com/conan-io/conan-center-index"
