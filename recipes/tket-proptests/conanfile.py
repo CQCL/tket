dd958f0dae224d019eb6fd9989bfd1d619f20c6b
sjdilkes <silas.dilkes@cambridgequantum.com>
2023-01-06 14:06:10 +0000

    bump

--- recipes/tket-proptests/conanfile.py
+++ recipes/tket-proptests/conanfile.py
@@ -28,7 +28,7 @@ class TketProptestsConan(ConanFile):
     generators = "cmake"
     exports_sources = "../../tket/proptests/*"
     requires = (
-        "tket/1.0.40@tket/stable",
+        "tket/1.0.42@tket/stable",
         "rapidcheck/cci.20220514",
     )
 
