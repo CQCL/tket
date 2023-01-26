9815dec3c6fbcaaa9717610e37095f95f12513b5
Yao Tang <yao.tang@cambridgequantum.com>
2023-01-26 11:17:02 +0000

    Implement state prep box

--- tket/tests/Circuit/test_StatePreparation.cpp
+++ tket/tests/Circuit/test_StatePreparation.cpp
@@ -20,6 +20,7 @@
 #include "Circuit/Boxes.hpp"
 #include "Circuit/CircUtils.hpp"
 #include "Circuit/Circuit.hpp"
+#include "Circuit/StatePreparation.hpp"
 #include "Eigen/src/Core/Matrix.h"
 #include "Gate/Rotation.hpp"
 #include "Simulation/CircuitSimulator.hpp"
@@ -57,6 +58,76 @@ SCENARIO("Test bloch sphere coordinates") {
     REQUIRE_THROWS_MATCHES(
         get_bloch_coordinate_from_state(a, b), std::invalid_argument,
         MessageContains("unnormalised"));
+  }
+}
+
+SCENARIO("Test state preparation") {
+  GIVEN("n qubit states") {
+    std::vector<Eigen::VectorXcd> test_states;
+    test_states.push_back(Eigen::Vector2cd(0, 1));
+    test_states.push_back(Eigen::Vector2cd(1, 0));
+    test_states.push_back(Eigen::Vector2cd(std::sqrt(0.5), std::sqrt(0.5)));
+    test_states.push_back(Eigen::Vector2cd(std::sqrt(0.5), -std::sqrt(0.5)));
+    test_states.push_back(Eigen::Vector4cd(0, 1, 0, 0));
+    test_states.push_back(Eigen::Vector4cd(0, 0, 0, 1));
+    test_states.push_back(Eigen::Vector4cd(1, 0, 0, 0));
+    test_states.push_back(Eigen::Vector4cd(
+        -std::sqrt(0.25), std::sqrt(0.25), std::sqrt(0.25), -std::sqrt(0.25)));
+    for (unsigned i = 0; i < 5; i++) {
+      test_states.push_back(random_state(8, i));
+      test_states.push_back(random_state(16, i));
+      test_states.push_back(random_state(32, i));
+      test_states.push_back(random_state(64, i));
+    }
+    for (auto psi : test_states) {
+      // test state prep is correct
+      StatePreparationBox prep(psi);
+      std::shared_ptr<Circuit> c = prep.to_circuit();
+      const Eigen::VectorXcd sv = tket_sim::get_statevector(*c);
+      REQUIRE((psi - sv).cwiseAbs().sum() < ERR_EPS);
+      // test the inverse of state prep is correct
+      StatePreparationBox inverse_prep(psi, true);
+      std::shared_ptr<Circuit> d = inverse_prep.to_circuit();
+      const Eigen::MatrixXcd U = tket_sim::get_unitary(*d);
+      const Eigen::VectorXcd final_state = U * psi;
+      // the final state should be the computational basis 0 state
+      REQUIRE(std::abs(Complex(1, 0) - final_state[0]) < ERR_EPS);
+      for (unsigned i = 1; i < psi.size(); i++) {
+        REQUIRE(std::abs(final_state[i]) < ERR_EPS);
+      }
+    }
+  }
+  GIVEN("unnormalised vector") {
+    Eigen::Vector2cd state(1, 1);
+    REQUIRE_THROWS_MATCHES(
+        StatePreparationBox(state), std::invalid_argument,
+        MessageContains("not normalised"));
+  }
+  GIVEN("vectors with wrong size") {
+    Eigen::VectorXcd state;
+    state << 1;
+    REQUIRE_THROWS_MATCHES(
+        StatePreparationBox(state), std::invalid_argument,
+        MessageContains("not a power of 2"));
+    Eigen::Vector3cd state2(1, 0, 0);
+    REQUIRE_THROWS_MATCHES(
+        StatePreparationBox(state2), std::invalid_argument,
+        MessageContains("not a power of 2"));
+  }
+  GIVEN("test dagger") {
+    Eigen::Vector2cd state(std::sqrt(0.5), -std::sqrt(0.5));
+    StatePreparationBox prep(state);
+    const StatePreparationBox dag_box =
+        static_cast<const StatePreparationBox &>(*prep.dagger());
+    REQUIRE((state - dag_box.get_statevector()).cwiseAbs().sum() < ERR_EPS);
+    REQUIRE(dag_box.is_inverse() == true);
+  }
+  GIVEN("copy constructor") {
+    Eigen::Vector2cd state(std::sqrt(0.5), -std::sqrt(0.5));
+    StatePreparationBox prep(state, true);
+    StatePreparationBox prep2(prep);
+    REQUIRE((state - prep2.get_statevector()).cwiseAbs().sum() < ERR_EPS);
+    REQUIRE(prep2.is_inverse() == true);
   }
 }
 
