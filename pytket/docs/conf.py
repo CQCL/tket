3daf8fd0e0407e7206fe5c4f2f81532f06c211e0
melf <cqc@melf.de>
2023-03-16 15:27:14 +0000

    update version in docs

--- pytket/docs/conf.py
+++ pytket/docs/conf.py
@@ -38,9 +38,9 @@ copyright = "2023, Cambridge Quantum Computing Ltd"
 author = "Cambridge Quantum Computing Ltd"
 
 # The short X.Y version
-version = "1.11"
+version = "1.13"
 # The full version, including alpha/beta/rc tags
-release = "1.11.1"
+release = "1.13.0"
 
 
 # -- General configuration ---------------------------------------------------
