a47f4b15a1f75ac951cb34c8f2972ec63e52b5c8
melf <cqc@melf.de>
2023-03-16 12:48:22 +0000

    update python version

--- pytket/setup.py
+++ pytket/setup.py
@@ -236,7 +236,7 @@ setup(
     name="pytket",
     author="TKET development team",
     author_email="tket-support@cambridgequantum.com",
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     project_urls={
         "Documentation": "https://cqcl.github.io/tket/pytket/api/index.html",
         "Source": "https://github.com/CQCL/tket",
