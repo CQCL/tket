abfa7a21406ada31cdbbcf32ff436e8275ab2d8c
Silas Dilkes <36165522+sjdilkes@users.noreply.github.com>
2023-03-24 13:27:05 +0000

    Update pytket/pytket/qasm/qasm.py
    Co-authored-by: Alec Edgington <54802828+cqc-alec@users.noreply.github.com>

--- pytket/pytket/qasm/qasm.py
+++ pytket/pytket/qasm/qasm.py
@@ -847,14 +847,10 @@ class CircuitTransformer(Transformer):
                 ],
             )
             # checks that each command has same string
-            existing_op = set(
-                [
-                    str(g) == str(c)
+            existing_op = all(str(g) == str(c)
                     for g, c in zip(
                         gate_circ.get_commands(), comparison_circ.get_commands()
-                    )
-                ]
-            ) == {True}
+                    ))
         if not existing_op:
             gate_circ.symbol_substitution(symbol_map)
             gate_circ.rename_units(rename_map)
