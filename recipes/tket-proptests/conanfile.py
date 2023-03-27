21f47f0c34d098938e8e3528e2e8455f52058eb2
Yao Tang <yao.tang@cambridgequantum.com>
2023-03-27 12:51:13 +0100

    bump tket lib version

--- recipes/tket-proptests/conanfile.py
+++ recipes/tket-proptests/conanfile.py
@@ -28,7 +28,7 @@ class TketProptestsConan(ConanFile):
     generators = "cmake"
     exports_sources = "../../tket/proptests/*"
     requires = (
-        "tket/1.0.58@tket/stable",
+        "tket/1.0.59@tket/stable",
         "rapidcheck/cci.20220514",
     )
 
