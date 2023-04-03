ac86b2d2a93f455eb8128667033bd4d1eefd05f0
Alec Edgington <alec.edgington@quantinuum.com>
2023-04-03 13:41:44 +0100

    Rework build files.

--- tket/conanfile.py
+++ tket/conanfile.py
@@ -12,16 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from conans import ConanFile, CMake, tools
-from conans.errors import ConanInvalidConfiguration
-import os
-import shutil
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
+from conan.errors import ConanInvalidConfiguration
 
 
 class TketConan(ConanFile):
     name = "tket"
     version = "1.1.2"
-    license = "CQC Proprietary"
+    package_type = "library"
+    license = "Apache 2"
     homepage = "https://github.com/CQCL/tket"
     url = "https://github.com/conan-io/conan-center-index"
     description = "Quantum SDK"
@@ -29,57 +29,31 @@ class TketConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     options = {
         "shared": [True, False],
+        "fPIC": [True, False],
         "profile_coverage": [True, False],
     }
-    default_options = {"shared": False, "profile_coverage": False}
-    generators = "cmake"
-    exports_sources = ["../../tket/src/*", "!*/build/*"]
-    requires = (
-        # libraries installed from remote:
-        # https://quantinuumsw.jfrog.io/artifactory/api/conan/tket1-libs
-        "boost/1.81.0",
-        "symengine/0.9.0",
-        "eigen/3.4.0",
-        "nlohmann_json/3.11.2",
-        "tklog/0.3.3@tket/stable",
-        "tkassert/0.3.3@tket/stable",
-        "tkrng/0.3.3@tket/stable",
-        "tktokenswap/0.3.3@tket/stable",
-        "tkwsm/0.3.3@tket/stable",
-    )
+    default_options = {"shared": False, "fPIC": True, "profile_coverage": False}
+    exports_sources = "CMakeLists.txt", "src/*", "include/*"
 
-    # List of components in a topological sort according to dependencies:
-    comps = [
-        "Utils",
-        "OpType",
-        "Clifford",
-        "Ops",
-        "Graphs",
-        "Gate",
-        "PauliGraph",
-        "Circuit",
-        "Architecture",
-        "Simulation",
-        "Diagonalisation",
-        "Characterisation",
-        "ZX",
-        "Converters",
-        "Placement",
-        "ArchAwareSynth",
-        "Mapping",
-        "MeasurementSetup",
-        "Transformations",
-        "Predicates",
-    ]
+    def config_options(self):
+        if self.settings.os == "Windows":
+            del self.options.fPIC
 
-    _cmake = None
+    def configure(self):
+        # Disable features that are still under the LGPL.
+        self.options["eigen"].MPL2_only = True
+        # Only need header libraries from boost.
+        self.options["boost"].header_only = True
 
-    def _configure_cmake(self):
-        if self._cmake is None:
-            self._cmake = CMake(self)
-            self._cmake.definitions["PROFILE_COVERAGE"] = self.options.profile_coverage
-            self._cmake.configure()
-        return self._cmake
+    def layout(self):
+        cmake_layout(self)
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.variables["PROFILE_COVERAGE"] = self.options.profile_coverage
+        tc.generate()
 
     def validate(self):
         if self.options.profile_coverage and self.settings.compiler != "gcc":
@@ -87,29 +61,27 @@ class TketConan(ConanFile):
                 "`profile_coverage` option only available with gcc"
             )
 
-    def configure(self):
-        # Disable features that are still under the LGPL.
-        self.options["eigen"].MPL2_only = True
-        # Use header-only boost libraries.
-        self.options["boost"].header_only = True
-
     def build(self):
-        cmake = self._configure_cmake()
+        cmake = CMake(self)
+        cmake.configure()
         cmake.build()
 
     def package(self):
-        self.copy("LICENSE", dst="licenses", src="../..")
-        for comp in self.comps:
-            self.copy(f"{comp}/include/*.hpp", dst=f"include/{comp}", keep_path=False)
-        self.copy("*.dll", dst="lib", keep_path=False)
-        self.copy("*.dll", dst="bin", keep_path=False)
-        self.copy("*.lib", dst="lib", keep_path=False)
-        self.copy("*.so", dst="lib", keep_path=False)
-        self.copy("*.dylib", dst="lib", keep_path=False)
-        self.copy("*.a", dst="lib", keep_path=False)
+        cmake = CMake(self)
+        cmake.install()
 
     def package_info(self):
-        # These must be ordered "top to bottom", so that when building statically
-        # unresolved symbols in libraries earlier in the list get resolved by later
-        # libraries.
-        self.cpp_info.libs = [f"tket-{comp}" for comp in reversed(self.comps)]
+        self.cpp_info.libs = ["tket"]
+
+    def requirements(self):
+        # libraries installed from remote:
+        # https://quantinuumsw.jfrog.io/artifactory/api/conan/tket1-libs
+        self.requires("boost/1.81.0", transitive_headers=True)
+        self.requires("symengine/0.9.0", transitive_headers=True)
+        self.requires("eigen/3.4.0", transitive_headers=True)
+        self.requires("nlohmann_json/3.11.2", transitive_headers=True)
+        self.requires("tklog/0.3.3@tket/stable")
+        self.requires("tkassert/0.3.3@tket/stable", transitive_headers=True)
+        self.requires("tkrng/0.3.3@tket/stable")
+        self.requires("tktokenswap/0.3.3@tket/stable")
+        self.requires("tkwsm/0.3.3@tket/stable")
--- tket/test_package/conanfile.py
+++ tket/test_package/conanfile.py
@@ -13,20 +13,28 @@
 # limitations under the License.
 
 import os
-from conans import ConanFile, CMake, tools
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.build import can_run
 
 
 class TketTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "cmake"
-    keep_imports = True
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
 
+    def layout(self):
+        cmake_layout(self)
+
     def test(self):
-        if not tools.cross_building(self):
-            bin_path = os.path.join("bin", "test")
-            self.run(bin_path, run_environment=True)
+        if can_run(self):
+            cmd = os.path.join(self.cpp.build.bindir, "test")
+            self.run(cmd, env="conanrun")
