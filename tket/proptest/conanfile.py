341c68c0913f7a56e32f21657317d3aa181d97ec
Alec Edgington <alec.edgington@quantinuum.com>
2023-05-12 17:45:34 +0100

    Bump tket version.

--- tket/proptest/conanfile.py
+++ tket/proptest/conanfile.py
@@ -47,5 +47,5 @@ class proptest_tketRecipe(ConanFile):
         cmake.install()
 
     def requirements(self):
-        self.requires("tket/1.2.8@tket/stable")
+        self.requires("tket/1.2.9@tket/stable")
         self.requires("rapidcheck/cci.20220514")
