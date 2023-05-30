16a27e77cc77f5c587ce17f0df7508e79ba7acae
yao-cqc <75305462+yao-cqc@users.noreply.github.com>
2023-05-30 12:53:00 +0100

    Bugfix/q control box not catching su2 (#876)
    * Allow errors when testing determinants
    
    * Add changelog entry
    
    * bump tket version

--- tket/conanfile.py
+++ tket/conanfile.py
@@ -19,7 +19,7 @@ from conan.errors import ConanInvalidConfiguration
 
 class TketConan(ConanFile):
     name = "tket"
-    version = "1.2.13"
+    version = "1.2.14"
     package_type = "library"
     license = "Apache 2"
     homepage = "https://github.com/CQCL/tket"
