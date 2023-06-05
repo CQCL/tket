bdedb6d22cb70306d5d7df787783d1aa15b1f9bb
Travis Thompson <travis.thompson@quantinuum.com>
2023-06-05 21:09:48 +0200

    use ninja if available

--- tket/conanfile.py
+++ tket/conanfile.py
@@ -13,6 +13,8 @@
 # limitations under the License.
 
 import os
+import subprocess
+
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 from conan.tools.files import copy
@@ -64,6 +66,12 @@ class TketConan(ConanFile):
         deps.generate()
         tc = CMakeToolchain(self)
         tc.variables["PROFILE_COVERAGE"] = self.options.profile_coverage
+        # use ninja generator if available
+        status_code, version = subprocess.getstatusoutput("ninja --version")
+        if status_code == 0:
+            tc.generator = "Ninja"
+            print(f"{os.linesep} --> Using Ninja generator, found version {version}{os.linesep}")
+        # propagate build test options
         if self.build_test():
             tc.variables["BUILD_TKET_TEST"] = True
             architectures_dir = os.path.join(self.source_folder, "test/src/test_architectures")
