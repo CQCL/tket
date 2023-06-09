47b5b2ac67ff356f80a15f21ad88cc5e36fb4363
Alec Edgington <alec.edgington@quantinuum.com>
2023-06-09 09:32:26 +0100

    Import Counter from typing.

--- pytket/pytket/utils/stats.py
+++ pytket/pytket/utils/stats.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from collections import Counter
+from typing import Counter
 from pytket.circuit import Circuit, OpType  # type: ignore
 
 
