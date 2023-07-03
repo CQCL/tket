ea9cbdeed1e8ca42213765a33ca42ee94af26008
Jake Arkinstall <jake.arkinstall@quantinuum.com>
2023-07-03 13:53:34 +0100

    Add default.nix to tktokenswap
    See d3ae2c96 (Added default.nix for tklog., 2023-07-03) for
    implementation details.

---
+++ libs/tktokenswap/default.nix
@@ -0,0 +1,21 @@
+{
+  pkgs ? import <nixpkgs> {},
+  tklog ? import ../tklog { inherit pkgs; },
+  tkrng ? import ../tkrng { inherit pkgs; },
+  tkassert ? import ../tkassert { inherit pkgs; inherit tklog; },
+}:
+let
+  src = builtins.filterSource(p: _: baseNameOf p != "default.nix") ./.;
+in
+  pkgs.stdenv.mkDerivation{
+    name = "tktokenswap";
+    inherit src;
+    buildInputs = [ pkgs.cmake pkgs.boost tklog tkassert tkrng ];
+    postFixup = ''
+      # fix bogus include paths
+      # trick found here: https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/libraries/crc32c/default.nix
+      for f in $(find $out/lib/cmake -name '*.cmake'); do
+        substituteInPlace "$f" --replace "\''${_IMPORT_PREFIX}/$out/include" "\''${_IMPORT_PREFIX}/include"
+      done
+    '';
+  }
