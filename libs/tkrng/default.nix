25c9083d0b7a76a6a28165a8a1e5229d2aacf207
Jake Arkinstall <jake.arkinstall@quantinuum.com>
2023-07-03 13:52:58 +0100

    Added default.nix to tkrng
    See d3ae2c96 (Added default.nix for tklog., 2023-07-03) for
    implementation details.

---
+++ libs/tkrng/default.nix
@@ -0,0 +1,16 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  src = builtins.filterSource(p: _: baseNameOf p != "default.nix") ./.;
+in
+  pkgs.stdenv.mkDerivation{
+    name = "tkrng";
+    inherit src;
+    buildInputs = [ pkgs.cmake ];
+    postFixup = ''
+      # fix bogus include paths
+      # trick found here: https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/libraries/crc32c/default.nix
+      for f in $(find $out/lib/cmake -name '*.cmake'); do
+        substituteInPlace "$f" --replace "\''${_IMPORT_PREFIX}/$out/include" "\''${_IMPORT_PREFIX}/include"
+      done
+    '';
+  }
