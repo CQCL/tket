a9c419dd893f99559c0abccdae8fe995518f406c
Jake Arkinstall <jake.arkinstall@quantinuum.com>
2023-07-03 13:47:28 +0100

    Added default.nix for tklog.
    Some key points to note about this implementation. First,
    the imported source files avoid default.nix itself such that
    changes to default.nix will not be considered changes to the
    project source, and thus won't trigger unnecessary rebuilds.
    
    Second, Nix+CMake awkwardly double-prepended the nix store
    path to the include directory, rendering it e.g.:
    /nix/store/[hash]-tklog//nix/store/[hash]-tklog
    To avoid this, a post phase is added to strip this unwanted
    behaviour, allowing it to be imported from other projects
    without failure.

---
+++ libs/tklog/default.nix
@@ -0,0 +1,27 @@
+{ pkgs ? import <nixpkgs> {} }:
+let
+  src = builtins.filterSource(p: _: baseNameOf p != "default.nix") ./.;
+in
+  pkgs.stdenv.mkDerivation{
+    name = "tklog";
+    inherit src;
+    buildInputs = [ pkgs.cmake ];
+    postFixup = ''
+      # fix bogus include paths
+      # trick found here: https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/libraries/crc32c/default.nix
+      # this is needed because when Nix builds the cmake project, it doubly prefixes the include paths:
+      # e.g.:
+      #  set(_IMPORT_PREFIX "/nix/store/wbvzlgvc7984xmpjd6lf3cg2490xb81b-tklog")
+      #  ...
+      #  target_sources(tklog::tklog
+      #    INTERFACE
+      #      FILE_SET "HEADERS"
+      #      TYPE "HEADERS"
+      #      BASE_DIRS "\''${_IMPORT_PREFIX}//nix/store/wbvzlgvc7984xmpjd6lf3cg2490xb81b-tklog/include"
+      #      FILES "\''${_IMPORT_PREFIX}//nix/store/wbvzlgvc7984xmpjd6lf3cg2490xb81b-tklog/include/tklog/TketLog.hpp"
+      #  )
+      for f in $(find $out/lib/cmake -name '*.cmake'); do
+        substituteInPlace "$f" --replace "\''${_IMPORT_PREFIX}/$out/include" "\''${_IMPORT_PREFIX}/include"
+      done
+    '';
+  }
