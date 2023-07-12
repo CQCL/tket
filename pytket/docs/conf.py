cf6d13917ffc268c5143f8639b9c83847d9faf32
Alec Edgington <alec.edgington@quantinuum.com>
2023-07-12 10:37:49 +0100

    Update version and changelog.

--- pytket/docs/conf.py
+++ pytket/docs/conf.py
@@ -40,7 +40,7 @@ author = "Quantinuum"
 # The short X.Y version
 version = "1.17"
 # The full version, including alpha/beta/rc tags
-release = "1.17.0"
+release = "1.17.1"
 
 
 # -- General configuration ---------------------------------------------------
