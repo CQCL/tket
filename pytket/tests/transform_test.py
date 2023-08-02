cf7198d6a2c365a7431417284c59533741699a30
sjdilkes <silas.dilkes@cambridgequantum.com>
2023-08-02 10:25:39 +0100

    Remove sycamore tests

--- pytket/tests/transform_test.py
+++ pytket/tests/transform_test.py
@@ -1230,15 +1230,15 @@ def test_auto_rebase_with_swap_cx() -> None:
     no_swap_pass.apply(c_no_swap)
     assert c_no_swap.n_gates_of_type(OpType.CX) == 2
 
-    c_swap = Circuit(2).Sycamore(0, 1)
-    swap_pass.apply(c_swap)
-    assert c_swap.n_gates_of_type(OpType.CX) == 1
-    iqp = c_swap.implicit_qubit_permutation()
-    assert iqp[Qubit(0)] == Qubit(1)
-    assert iqp[Qubit(1)] == Qubit(0)
-    c_no_swap = Circuit(2).Sycamore(0, 1)
-    no_swap_pass.apply(c_no_swap)
-    assert c_no_swap.n_gates_of_type(OpType.CX) == 3
+    # c_swap = Circuit(2).Sycamore(0, 1)
+    # swap_pass.apply(c_swap)
+    # assert c_swap.n_gates_of_type(OpType.CX) == 1
+    # iqp = c_swap.implicit_qubit_permutation()
+    # assert iqp[Qubit(0)] == Qubit(1)
+    # assert iqp[Qubit(1)] == Qubit(0)
+    # c_no_swap = Circuit(2).Sycamore(0, 1)
+    # no_swap_pass.apply(c_no_swap)
+    # assert c_no_swap.n_gates_of_type(OpType.CX) == 3
 
     c_swap = Circuit(2).ISWAP(0.3, 0, 1)
     swap_pass.apply(c_swap)
@@ -1295,18 +1295,18 @@ def test_auto_rebase_with_swap_zzmax() -> None:
     assert c_no_swap.n_gates_of_type(OpType.ZZMax) == 2
     assert c_no_swap.n_gates == 13
 
-    c_swap = Circuit(2).Sycamore(0, 1)
-    swap_pass.apply(c_swap)
-    assert c_swap.n_gates_of_type(OpType.ZZMax) == 1
-    assert c_swap.n_gates == 12
-    iqp = c_swap.implicit_qubit_permutation()
-    assert iqp[Qubit(0)] == Qubit(1)
-    assert iqp[Qubit(1)] == Qubit(0)
+    # c_swap = Circuit(2).Sycamore(0, 1)
+    # swap_pass.apply(c_swap)
+    # assert c_swap.n_gates_of_type(OpType.ZZMax) == 1
+    # assert c_swap.n_gates == 12
+    # iqp = c_swap.implicit_qubit_permutation()
+    # assert iqp[Qubit(0)] == Qubit(1)
+    # assert iqp[Qubit(1)] == Qubit(0)
 
-    c_no_swap = Circuit(2).Sycamore(0, 1)
-    no_swap_pass.apply(c_no_swap)
-    assert c_no_swap.n_gates_of_type(OpType.ZZMax) == 3
-    assert c_no_swap.n_gates == 16
+    # c_no_swap = Circuit(2).Sycamore(0, 1)
+    # no_swap_pass.apply(c_no_swap)
+    # assert c_no_swap.n_gates_of_type(OpType.ZZMax) == 3
+    # assert c_no_swap.n_gates == 16
 
     c_swap = Circuit(2).ISWAP(0.3, 0, 1)
     swap_pass.apply(c_swap)
@@ -1365,18 +1365,18 @@ def test_auto_rebase_with_swap_zzphase() -> None:
     assert c_no_swap.n_gates_of_type(OpType.ZZPhase) == 2
     assert c_no_swap.n_gates == 13
 
-    c_swap = Circuit(2).Sycamore(0, 1)
-    swap_pass.apply(c_swap)
-    assert c_swap.n_gates_of_type(OpType.ZZPhase) == 1
-    assert c_swap.n_gates == 12
-    iqp = c_swap.implicit_qubit_permutation()
-    assert iqp[Qubit(0)] == Qubit(1)
-    assert iqp[Qubit(1)] == Qubit(0)
+    # c_swap = Circuit(2).Sycamore(0, 1)
+    # swap_pass.apply(c_swap)
+    # assert c_swap.n_gates_of_type(OpType.ZZPhase) == 1
+    # assert c_swap.n_gates == 12
+    # iqp = c_swap.implicit_qubit_permutation()
+    # assert iqp[Qubit(0)] == Qubit(1)
+    # assert iqp[Qubit(1)] == Qubit(0)
 
-    c_no_swap = Circuit(2).Sycamore(0, 1)
-    no_swap_pass.apply(c_no_swap)
-    assert c_no_swap.n_gates_of_type(OpType.ZZPhase) == 3
-    assert c_no_swap.n_gates == 16
+    # c_no_swap = Circuit(2).Sycamore(0, 1)
+    # no_swap_pass.apply(c_no_swap)
+    # assert c_no_swap.n_gates_of_type(OpType.ZZPhase) == 3
+    # assert c_no_swap.n_gates == 16
 
     c_swap = Circuit(2).ISWAP(0.3, 0, 1)
     swap_pass.apply(c_swap)
