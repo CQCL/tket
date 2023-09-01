905c807aecac49ed4a75f6676778f865f78a0c5c
Travis Thompson <travis.thompson@quantinuum.com>
2023-09-01 13:37:18 +0200

    don't pass enum at all

--- pytket/pytket/utils/graph.py
+++ pytket/pytket/utils/graph.py
@@ -61,7 +61,7 @@ class Graph:
         self.Gnx: Optional[nx.MultiDiGraph] = None
         self.G: Optional[gv.Digraph] = None
         self.Gqc: Optional[gv.Graph] = None
-        self.edge_data: dict[tuple[int, int], list[tuple[int, int, EdgeType]]] = defaultdict(list)
+        self.edge_data: dict[tuple[int, int], list[tuple[int, int, str]]] = defaultdict(list)
         self.port_counts: dict = defaultdict(int)
         for src_node, tgt_node, src_port, tgt_port, edge_type in edge_data:
             self.edge_data[(src_node, tgt_node)].append((src_port, tgt_port, edge_type))
@@ -87,7 +87,7 @@ class Graph:
                     tgt_node,
                     src_port=src_port,
                     tgt_port=tgt_port,
-                    edge_type=edge_type.value,
+                    edge_type=edge_type,
                 )
 
         # Add node IDs to edges
@@ -223,10 +223,10 @@ class Graph:
                         for i in range(n_ports):
                             c.node(name=str((node, i)), xlabel=str(i), **port_node_attr)
         edge_colors = {
-            EdgeType.Quantum.value: q_color,
-            EdgeType.Boolean.value: b_color,
-            EdgeType.Classical.value: c_color,
-            EdgeType.WASM.value: w_color,
+            "Quantum": q_color,
+            "Boolean": b_color,
+            "Classical": c_color,
+            "WASM": w_color,
         }
         edge_attr = {
             "weight": "2",
