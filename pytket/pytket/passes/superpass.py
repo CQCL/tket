43c646229635e98aa124f91a6d7f362e3da59c22
Melf <cqc@melf.de>
2023-09-22 15:23:15 +0100

    fix lint

--- pytket/pytket/passes/superpass.py
+++ pytket/pytket/passes/superpass.py
@@ -12,10 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from pytket.circuit import Circuit
-from .._tket.passes import BasePass
 import copy
 from typing import Optional, Callable
+
+from pytket.circuit import Circuit
+from .._tket.passes import BasePass
 
 
 class superpass:
