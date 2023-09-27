b07752343fcc9079e554424f5adfe44f7284b46d
Melf <cqc@melf.de>
2023-09-27 19:14:42 +0100

    add aditional check for index out of array functions

--- pytket/pytket/wasm/wasm.py
+++ pytket/pytket/wasm/wasm.py
@@ -143,13 +143,16 @@ class WasmFileHandler:
         for x in function_names:
             # check for only integer type in parameters and return values
             supported_function = True
-            idx = _func_lookup[x][1]
-            for t in function_signatures[self._function_types[idx]]["parameter_types"]:
-                if t != self._int_type:
-                    supported_function = False
-            for t in function_signatures[self._function_types[idx]]["return_types"]:
-                if t != self._int_type:
-                    supported_function = False
+            idx = _func_lookup[x][0]
+            if idx < len(self._function_types):
+                for t in function_signatures[self._function_types[idx]]["parameter_types"]:
+                    if t != self._int_type:
+                        supported_function = False
+                for t in function_signatures[self._function_types[idx]]["return_types"]:
+                    if t != self._int_type:
+                        supported_function = False
+            else:
+                supported_function = False
 
             if supported_function:
                 self._functions[x] = (
