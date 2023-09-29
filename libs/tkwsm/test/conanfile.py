a7c5a1437c2d8e50e5f6105554fc4b216e62a184
Melf <cqc@melf.de>
2023-09-29 12:23:59 +0100

    update tktokenswap version to 0.3.6

--- libs/tkwsm/test/conanfile.py
+++ libs/tkwsm/test/conanfile.py
@@ -59,7 +59,7 @@ class test_tkwsmRecipe(ConanFile):
         cmake.install()
 
     def requirements(self):
-        self.requires("tkwsm/0.3.5")
+        self.requires("tkwsm/0.3.6")
         self.requires("tkassert/0.3.3@tket/stable")
         self.requires("tkrng/0.3.3@tket/stable")
         self.requires("catch2/3.3.2")
