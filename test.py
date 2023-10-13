e1f8324f77e358ec243dd150c067e9dbbf2ca3f6
Travis Thompson <travis.thompson@quantinuum.com>
2023-10-13 13:30:17 +0200

    try with python shell

--- test.py
+++ test.py
@@ -1,7 +1,12 @@
+import os
 import subprocess
 import re
 
-arch = str(subprocess.check_output("cat testsssssss", shell=True))
-print(arch)
-arch = re.search(r"C:\\+ProgramData\\+Chocolatey\\+lib\\+ccache\\+tools\\+ccache-4\.8\.3-windows-x86_64\\+ccache\.exe", arch)
-print(arch.group())
+try:
+    shimgen_help = subprocess.check_output("cat testsssssss", shell=True, text=True)
+except Exception:
+    pass
+ccache_bin = re.search(r"C:\\+ProgramData\\+Chocolatey\\+lib\\+ccache\\+tools\\+ccache-4\.8\.3-windows-x86_64\\+ccache\.exe", shimgen_help).group()
+print(f"Found ccache binary at {ccache_bin}")
+#with open(os.environ["GITHUB_OUTPUT"], "a") as f:
+#    print("{0}={1}".format("ccache_binary", ccache_bin), file=f)
