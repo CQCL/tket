9867c4355c77726973a5ddbf8e5ec319404e3ab9
Alec Edgington <alec.edgington@quantinuum.com>
2024-01-03 13:31:27 +0000

    Bump rapidcheck version.

--- tket/conanfile.py
+++ tket/conanfile.py
@@ -123,7 +123,7 @@ class TketConan(ConanFile):
         if self.build_test():
             self.test_requires("catch2/3.5.1")
         if self.build_proptest():
-            self.test_requires("rapidcheck/cci.20220514")
+            self.test_requires("rapidcheck/cci.20230815")
 
     def build_test(self):
         return self.options.with_test or self.options.with_all_tests
