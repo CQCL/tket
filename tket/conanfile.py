2f18bd45632b7ee2b784d07bb46788755aa0c53e
cqc-melf <70640934+cqc-melf@users.noreply.github.com>
2024-01-12 09:38:45 +0000

    Update/copyright year (#1211)
    * updtae copyright year
    
    * update tket version
    
    * update tket version II

--- tket/conanfile.py
+++ tket/conanfile.py
@@ -1,4 +1,4 @@
-# Copyright 2019-2023 Cambridge Quantum Computing
+# Copyright 2019-2024 Cambridge Quantum Computing
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -23,7 +23,7 @@ from conan.errors import ConanInvalidConfiguration
 
 class TketConan(ConanFile):
     name = "tket"
-    version = "1.2.85"
+    version = "1.2.86"
     package_type = "library"
     license = "Apache 2"
     homepage = "https://github.com/CQCL/tket"
