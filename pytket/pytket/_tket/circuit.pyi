5964a5cabb4808a47ddc9260370ffe17271f3d7c
sjdilkes <silas.dilkes@cambridgequantum.com>
2024-03-14 10:23:48 +0000

    Update circuit.pyi

--- pytket/pytket/_tket/circuit.pyi
+++ pytket/pytket/_tket/circuit.pyi
@@ -3704,7 +3704,7 @@ class StatePreparationBox(Op):
         """
 class TermSequenceBox(Op):
     """
-    An unordered collection of Pauli exponentials that can be synthesised in any order, causing a change in the unitary operation. Synthesise order depends on the synthesis strategy chosen only.
+    An unordered collection of Pauli exponentials that can be synthesised in any order, causing a change in the unitary operation. Synthesis order depends on the synthesis strategy chosen only.
     """
     def __init__(self, pauli_gadgets: typing.Sequence[tuple[typing.Sequence[pytket._tket.pauli.Pauli], sympy.Expr | float]], synthesis_strategy: pytket._tket.transform.PauliSynthStrat = pytket._tket.transform.PauliSynthStrat.Sets, partitioning_strategy: pytket._tket.partition.PauliPartitionStrat = pytket._tket.partition.PauliPartitionStrat.CommutingSets, graph_colouring: pytket._tket.partition.GraphColourMethod = pytket._tket.partition.GraphColourMethod.Lazy, cx_config_type: CXConfigType = CXConfigType.Tree) -> None:
         """
