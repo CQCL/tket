6a3bb3878e1c7dbcbb87ceef34c3819dc2cd8d78
Alec Edgington <alec.edgington@quantinuum.com>
2024-04-04 11:54:48 +0100

    Restrict scipy version to < 1.13.0.

--- pytket/setup.py
+++ pytket/setup.py
@@ -194,7 +194,7 @@ setup(
         "sympy ~=1.6",
         "numpy >=1.21.4, <2.0",
         "lark-parser ~=0.7",
-        "scipy >=1.7.2, <2.0",
+        "scipy ~=1.12.0",
         "networkx >= 2.8.8",
         "graphviz ~= 0.14",
         "jinja2 ~= 3.0",
