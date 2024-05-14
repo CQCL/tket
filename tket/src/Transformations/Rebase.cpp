4495aad51ac58cf23c516c0d9b007e476fe6e44c
michael ontiveros <michael.david.ontiveros@gmail.com>
2024-05-14 13:24:04 -0600

    Reformat.

--- tket/src/Transformations/Rebase.cpp
+++ tket/src/Transformations/Rebase.cpp
@@ -291,8 +291,8 @@ Transform rebase_UMD() {
 // Singleqs: GPI, GPI2
 Transform rebase_ionq() {
   return rebase_factory_via_tk2(
-      {OpType::GPI, OpType::GPI2, OpType::AAMS},
-      CircPool::TK1_using_GPI, CircPool::TK2_using_AAMS);
+      {OpType::GPI, OpType::GPI2, OpType::AAMS}, CircPool::TK1_using_GPI, 
+      CircPool::TK2_using_AAMS);
 }
 
 }  // namespace Transforms
