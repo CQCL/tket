8aa12f140dfdf8959712fa6fce90669222038e66
cqc-melf <70640934+cqc-melf@users.noreply.github.com>
2024-05-14 16:27:26 +0100

    update tket email (#1391)

--- pytket/setup.py
+++ pytket/setup.py
@@ -177,7 +177,7 @@ class bdist_wheel(_bdist_wheel):
 setup(
     name="pytket",
     author="TKET development team",
-    author_email="tket-support@cambridgequantum.com",
+    author_email="tket-support@quantinuum.com",
     python_requires=">=3.10",
     project_urls={
         "Documentation": "https://tket.quantinuum.com/api-docs/index.html",
