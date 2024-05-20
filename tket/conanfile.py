ecc985550a7431fe118a114c770192f41a5805c7
Alec Edgington <54802828+cqc-alec@users.noreply.github.com>
2024-05-20 10:55:32 +0100

    Use `ubuntu-24.04` on CI (#1401)

--- tket/conanfile.py
+++ tket/conanfile.py
@@ -23,7 +23,7 @@ from conan.errors import ConanInvalidConfiguration
 
 class TketConan(ConanFile):
     name = "tket"
-    version = "1.2.127"
+    version = "1.3.0"
     package_type = "library"
     license = "Apache 2"
     homepage = "https://github.com/CQCL/tket"
