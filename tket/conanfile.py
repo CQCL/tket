d84f914ae1d5aceead80d95e0f84a1a58b0ed224
Melf <cqc@melf.de>
2024-06-07 11:11:58 +0100

    update tket version

--- tket/conanfile.py
+++ tket/conanfile.py
@@ -23,7 +23,7 @@ from conan.errors import ConanInvalidConfiguration
 
 class TketConan(ConanFile):
     name = "tket"
-    version = "1.3.4"
+    version = "1.3.5"
     package_type = "library"
     license = "Apache 2"
     homepage = "https://github.com/CQCL/tket"
