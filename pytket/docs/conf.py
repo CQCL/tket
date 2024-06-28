abcaf26b1b4f6536dee754aa7473bcfe98ae5e43
aidanCQ <aidan.keay@quantinuum.com>
2024-06-28 16:39:44 +0100

    Attempt to fix paths in conf.py.

--- pytket/docs/conf.py
+++ pytket/docs/conf.py
@@ -91,8 +91,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 html_theme = "furo"
-templates_path = ["qui-sphinx/_templates"]
-html_static_path = ['qui-sphinx/_static', '_static']
+templates_path = ["./qui-sphinx/_templates/"]
+html_static_path = ['./qui-sphinx/_static/', '_static/']
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
