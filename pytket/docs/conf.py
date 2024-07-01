aa09a0a4c4e0b8b07ffedcbbb639a9638ea51228
aidanCQ <aidan.keay@quantinuum.com>
2024-07-01 14:14:01 +0100

    Fix paths.

--- pytket/docs/conf.py
+++ pytket/docs/conf.py
@@ -89,8 +89,8 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 # a list of builtin themes.
 #
 html_theme = "furo"
-templates_path = ["./qui-sphinx/_templates/"]
-html_static_path = ["./qui-sphinx/_static/", "_static/"]
+templates_path = ["./quantinuum-sphinx/_templates/"]
+html_static_path = ["./quantinuum-sphinx/_static/", "_static/"]
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
