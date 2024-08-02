cb45898e437388b987aee90ee9b588127dd1ce8e
Melf <cqc@melf.de>
2024-08-02 11:52:44 +0100

    update version in docs

--- pytket/docs/conf.py
+++ pytket/docs/conf.py
@@ -38,9 +38,9 @@ copyright = "2024 Quantinuum"
 author = "Quantinuum"
 
 # The short X.Y version
-version = "1.30"
+version = "1.31"
 # The full version, including alpha/beta/rc tags
-release = "1.30.0"
+release = "1.31.0"
 
 
 # -- General configuration ---------------------------------------------------
