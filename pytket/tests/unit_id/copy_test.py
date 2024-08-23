fd3196db9bc72b2c7b01e85cdfd8860c16d09033
John Children <john.children@cambridgequantum.com>
2024-08-23 14:30:56 +0100

    fix: Add copy implementations for Unit IDs
    Prevent a bug where the wrong instance would be returned when copying or
    deep copying some of the unit id classes.

---
+++ pytket/tests/unit_id/copy_test.py
@@ -0,0 +1,49 @@
+# Copyright 2019-2024 Cambridge Quantum Computing
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from copy import copy, deepcopy
+
+from pytket.unit_id import Bit, Qubit, Node
+
+
+def test_copying_qubits():
+    q = Qubit(0)
+
+    q1 = copy(q)
+    q2 = deepcopy(q)
+
+    assert type(q) is Qubit
+    assert type(q1) is Qubit
+    assert type(q2) is Qubit
+
+
+def test_copying_bits():
+    b = Bit(0)
+
+    b1 = copy(b)
+    b2 = deepcopy(b)
+
+    assert type(b) is Qubit
+    assert type(b1) is Qubit
+    assert type(b2) is Qubit
+
+
+def test_copying_nodes():
+    n = Node(0)
+
+    n1 = copy(n)
+    n2 = deepcopy(n)
+
+    assert type(n) is Node
+    assert type(n1) is Node
+    assert type(n2) is Node
