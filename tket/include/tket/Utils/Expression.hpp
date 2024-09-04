35730c66a94e206511ebdf30d74b3f4bc94ca40f
Alec Edgington <alec.edgington@quantinuum.com>
2024-09-04 15:15:23 +0100

    Delete explicit deduction guide.

--- tket/include/tket/Utils/Expression.hpp
+++ tket/include/tket/Utils/Expression.hpp
@@ -33,9 +33,6 @@ template <class... Ts>
 struct overloaded : Ts... {
   using Ts::operator()...;
 };
-// explicit deduction guide (not needed as of C++20)
-template <class... Ts>
-overloaded(Ts...) -> overloaded<Ts...>;
 
 namespace tket {
 
