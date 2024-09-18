ce3f8a321e5bbd91e67719a957aad8f5c7834e38
Alec Edgington <alec.edgington@quantinuum.com>
2024-09-18 14:47:59 +0100

    Revert "Route circuit with maps."
    This reverts commit eaa9d25cbdced2319a0bdfe7544c62687e02321a.

--- tket/src/Predicates/PassGenerators.cpp
+++ tket/src/Predicates/PassGenerators.cpp
@@ -487,12 +487,11 @@ PassPtr gen_cx_mapping_pass(
   if (delay_measures) return_pass = return_pass >> DelayMeasures();
   return_pass = return_pass >> rebase_pass >>
                 gen_decompose_routing_gates_to_cxs_pass(arc, directed_cx);
-  Transform t{[=](Circuit& circ, std::shared_ptr<unit_bimaps_t> maps) {
-    MappingManager mm(std::make_shared<Architecture>(arc));
+  Transform t{[=](Circuit& circ) {
     CompilationUnit cu(circ);
     bool changed = return_pass->apply(cu);
     circ = cu.get_circ_ref();
-    return changed || mm.route_circuit_with_maps(circ, config, maps);
+    return changed;
   }};
   PassConditions conditions = return_pass->get_conditions();
   nlohmann::json j;
