e31f2269b6cbc96af3269a99137c786c09fdf49f
Alec Edgington <54802828+cqc-alec@users.noreply.github.com>
2024-09-27 16:17:14 +0100

    Alphabetize lists in cmake and conan files (#1594)

--- tket/conanfile.py
+++ tket/conanfile.py
@@ -23,7 +23,7 @@ from conan.errors import ConanInvalidConfiguration
 
 class TketConan(ConanFile):
     name = "tket"
-    version = "1.3.28"
+    version = "1.3.29"
     package_type = "library"
     license = "Apache 2"
     homepage = "https://github.com/CQCL/tket"
@@ -112,11 +112,11 @@ class TketConan(ConanFile):
         # libraries installed from remote:
         # https://quantinuumsw.jfrog.io/artifactory/api/conan/tket1-libs
         self.requires("boost/1.86.0", transitive_headers=True)
-        self.requires("symengine/0.12.0", transitive_headers=True)
         self.requires("eigen/3.4.0", transitive_headers=True)
         self.requires("nlohmann_json/3.11.3", transitive_headers=True)
-        self.requires("tklog/0.3.3@tket/stable")
+        self.requires("symengine/0.12.0", transitive_headers=True)
         self.requires("tkassert/0.3.4@tket/stable", transitive_headers=True)
+        self.requires("tklog/0.3.3@tket/stable")
         self.requires("tkrng/0.3.3@tket/stable")
         self.requires("tktokenswap/0.3.9@tket/stable")
         self.requires("tkwsm/0.3.9@tket/stable")
