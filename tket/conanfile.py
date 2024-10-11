dba6f7d11e2dea93008407dae9753087159271da
Alec Edgington <alec.edgington@quantinuum.com>
2024-10-11 14:36:30 +0100

    Bump tket version.

--- tket/conanfile.py
+++ tket/conanfile.py
@@ -23,7 +23,7 @@ from conan.errors import ConanInvalidConfiguration
 
 class TketConan(ConanFile):
     name = "tket"
-    version = "1.3.32"
+    version = "1.3.33"
     package_type = "library"
     license = "Apache 2"
     homepage = "https://github.com/CQCL/tket"
