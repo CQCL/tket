18baeb72598a93747fd98fadad92a013d4091522
Silas Dilkes <36165522+sjdilkes@users.noreply.github.com>
2024-10-28 12:56:05 +0000

    add groundwork for lightsabre pass in new binders "extras"

---
+++ pytket/binders/extras.cpp
@@ -0,0 +1,91 @@
+// Copyright 2019-2024 Cambridge Quantum Computing
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <pybind11/functional.h>
+
+#include <optional>
+#include <tklog/TketLog.hpp>
+
+#include "binder_json.hpp"
+#include "tket/Architecture/Architecture.hpp"
+#include "tket/Predicates/CompilerPass.hpp"
+#include "tket/Predicates/PassGenerators.hpp"
+#include "tket/Predicates/PassLibrary.hpp"
+#include "tket/Transformations/Transform.hpp"
+#include "typecast.hpp"
+
+namespace tket {
+    
+namespace py = pybind11;
+using json = nlohmann::json;
+
+Transform lightsabre_transform(const Architecture& arc, unsigned seed, unsigned, optimisation_level) {
+  return Transform([=](Circuit &circ) {
+    py::module lightsabre_module =
+        py::module::import("pytket.extras.lightsabre");
+    py::object lightsabre_transform =
+        lightsabre_module.attr("_gen_lightsabre_transformation");
+    circ = lightsabre_transform(circ, optimisation_level, seed).cast<Circuit>();
+    return true;
+  });
+}
+
+PassPtr lightsabre_routing(const Architecture& arc, unsigned seed, unsigned, optimisation_level){
+    // construct pass
+    Transform t = lightsabre_transform(arc, seed, optimisation_level) >> Transforms::decompose_CX_directed(arc);
+    PassPtr lightsabre = std::make_shared<StandardPass>(precons, t, postcons, j);
+    PassPtr rebase_pass = gen_auto_rebase_pass({OpType::CX, OpType::SX, OpType::Rz, OpType::X, OpType::TK1}, false);
+
+    // construct pre-conditions
+    PredicatePtr twoqbpred = std::make_shared<MaxTwoQubitGatesPredicate>();
+    PredicatePtr n_qubit_pred =
+        std::make_shared<MaxNQubitsPredicate>(arc.n_nodes());
+    PredicatePtrMap precons{
+        CompilationUnit::make_type_pair(twoqbpred),
+        CompilationUnit::make_type_pair(n_qubit_pred)};
+
+// construct post-conditions
+  PredicatePtr postcon1 = std::make_shared<ConnectivityPredicate>(arc);
+  std::pair<const std::type_index, PredicatePtr> pair1 =
+      CompilationUnit::make_type_pair(postcon1);
+  PredicatePtr postcon2 = std::make_shared<NoWireSwapsPredicate>();
+  PredicatePtrMap s_postcons{pair1, CompilationUnit::make_type_pair(postcon2)};
+  std::type_index gateset_ti = typeid(GateSetPredicate);
+
+  PredicateClassGuarantees g_postcons{
+      {pair1.first, Guarantee::Clear},
+      {gateset_ti, Guarantee::Clear},
+      {typeid(MaxTwoQubitGatesPredicate), Guarantee::Clear}};
+  PostConditions pc{s_postcons, g_postcons, Guarantee::Preserve};
+
+// config for json
+nlohmann::json j;
+j["name"] = "LightSABREPass";
+j["architecture"] = arc;
+j["seed"] = seed;
+j["optimisation_level"] = "optimisation_level"
+    return std::make_shared<StandardPass>(precons, rebase_pass >> lightsabre, pc, j);
+}
+
+PYBIND11_MODULE(extras, m){
+    py::module_::import("pytket._tket.passes");
+    m.def(
+        "LightSABRE", &lightsabre_routing,
+        "Routes circuits to a given architecture using the LightSABRE method "
+        "available in Qiskit.",
+        py::arg("architecture"), py::arg("seed")=0, py::arg("optimisation_level")=2;
+    )
+}
+
+} // namespace tket
---
+++ pytket/pytket/extras/lightsabre.py
@@ -0,0 +1,89 @@
+# Copyright 2019-2024 Quantinuum
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Methods to allow conversion between Qiskit and pytket circuit classes"""
+
+from typing import (
+    Callable,
+    List,
+    Tuple,
+)
+
+from qiskit import (
+    QuantumCircuit,
+)
+
+from qiskit.transpiler import PassManager, CouplingMap  # type: ignore
+from qiskit.transpiler.preset_passmanagers.builtin_plugins import SabreLayoutPassManager  # type: ignore
+from qiskit.transpiler.passmanager_config import PassManagerConfig  # type: ignore
+
+from pytket.circuit import (
+    Circuit,
+    Node,
+)
+
+from pytket.architecture import Architecture
+
+def _architecture_to_couplingmap(architecture: Architecture) -> CouplingMap:
+    """
+    Converts a pytket Architecture object to a Qiskit CouplingMap object.
+
+    :param architecture: Architecture to be converted
+    """
+    # we can make some assumptions from how the Architecture object is
+    # originally constructed from the Qiskit CouplingMap:
+    # 1) All nodes are single indexed
+    # 2) All nodes are default register
+    # 3) Node with index "i" corresponds to integer "i" in the original coupling map
+    # We confirm assumption 1) and 2) while producing the coupling map
+    coupling_map: List[Tuple[int, int]] = []
+    for edge in architecture.coupling:
+        assert len(edge[0].index) == 1
+        assert len(edge[1].index) == 1
+        assert edge[0].reg_name == "node"
+        assert edge[1].reg_name == "node"
+        coupling_map.append((edge[0].index[0], edge[1].index[0]))
+    return CouplingMap(coupling_map)
+
+
+def _gen_lightsabre_transformation(
+    architecture: Architecture,
+    optimisation_level: int = 2,
+    seed=0,
+) -> Callable[Circuit, Circuit]:
+    """
+    Generates a function that can be passed to CustomPass for running
+    LightSABRE routing.
+
+    :param coupling_map: Architecture LightSABRE routes circuits to match
+    :param optimisation_level: Corresponds to qiskit optmization levels
+    :param seed: LightSABRE routing is stochastic, with this parameter setting the seed
+    """
+    config: PassManagerConfig = PassManagerConfig(
+        coupling_map=_architecture_to_couplingmap(architecture),
+        routing_method="sabre",
+        seed_transpiler=seed,
+    )
+
+    def lightsabre(circuit: Circuit) -> Circuit:
+        sabre_pass: PassManager = SabreLayoutPassManager().pass_manager(
+            config, optimisation_level=optimisation_level
+        )
+        c: Circuit = qiskit_to_tk(sabre_pass.run(tk_to_qiskit(circuit)))
+        c.remove_blank_wires()
+        c.rename_units({q: Node(q.index[0]) for q in c.qubits})
+        return c
+
+    return lightsabre
