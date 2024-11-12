5055846dd6387c15be3ab3798fd0193f3bdda283
Alec Edgington <54802828+cqc-alec@users.noreply.github.com>
2024-11-12 09:05:47 +0000

    Update to symengine 0.13.0 (#1666)

--- tket/conanfile.py
+++ tket/conanfile.py
@@ -23,7 +23,7 @@ from conan.errors import ConanInvalidConfiguration
 
 class TketConan(ConanFile):
     name = "tket"
-    version = "1.3.45"
+    version = "1.3.46"
     package_type = "library"
     license = "Apache 2"
     homepage = "https://github.com/CQCL/tket"
@@ -114,7 +114,7 @@ class TketConan(ConanFile):
         self.requires("boost/1.86.0", transitive_headers=True)
         self.requires("eigen/3.4.0", transitive_headers=True)
         self.requires("nlohmann_json/3.11.3", transitive_headers=True)
-        self.requires("symengine/0.12.0", transitive_headers=True)
+        self.requires("symengine/0.13.0", transitive_headers=True)
         self.requires("tkassert/0.3.4@tket/stable", transitive_headers=True)
         self.requires("tklog/0.3.3@tket/stable")
         self.requires("tkrng/0.3.3@tket/stable")
