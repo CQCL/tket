042ee0205717e3dc3cee073b49c6c81d7afc5f57
Silas Dilkes <36165522+sjdilkes@users.noreply.github.com>
2024-11-18 17:32:40 +0000

    bump and regen stubs

--- pytket/pytket/_tket/transform.pyi
+++ pytket/pytket/_tket/transform.pyi
@@ -165,7 +165,7 @@ class Transform:
         It is not recommended to use this transformation with symbolic expressions, as in certain cases a blow-up in symbolic expression sizes may occur.
         """
     @staticmethod
-    def GreedyPauliSimp(discount_rate: float = 0.7, depth_weight: float = 0.3, max_tqe_candidates: int = 500, max_lookahead: int = 500, seed: int = 0, allow_zzphase: bool = False, timeout: int = 100) -> Transform:
+    def GreedyPauliSimp(discount_rate: float = 0.7, depth_weight: float = 0.3, max_tqe_candidates: int = 500, max_lookahead: int = 500, seed: int = 0, allow_zzphase: bool = False, thread_timeout: int = 100, trials: int = 1) -> Transform:
         """
         Convert a circuit into a graph of Pauli gadgets to account for commutation and phase folding, and resynthesises them using a greedy algorithm adapted from arxiv.org/abs/2103.08602. The method for synthesising the final Clifford operator is adapted from arxiv.org/abs/2305.10966.
         
@@ -175,7 +175,8 @@ class Transform:
         :param max_lookahead:  Maximum lookahead when evaluating each Clifford gate candidate. Default to 500.
         :param seed:  Unsigned integer seed used for sampling candidates and tie breaking. Default to 0.
         :param allow_zzphase: If set to True, allows the algorithm to implement 2-qubit rotations using ZZPhase gates when deemed optimal. Defaults to False.
-        :param timeout: Sets maximum out of time spent finding solution.
+        :param thread_timeout: Sets maximum out of time spent finding a single solution in one thread.
+        :param trials: Sets maximum number of found solutions.
         :return: a pass to perform the simplification
         """
     @staticmethod
