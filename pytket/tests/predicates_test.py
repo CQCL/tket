26970b6995932cc456b370fb819ba03fa4fcb389
Silas Dilkes <36165522+sjdilkes@users.noreply.github.com>
2024-11-19 14:45:04 +0000

    Update predicates_test.py

--- pytket/tests/predicates_test.py
+++ pytket/tests/predicates_test.py
@@ -1035,7 +1035,6 @@ def test_greedy_pauli_synth() -> None:
         rega[0], regb[0]
     ).SWAP(regb[1], rega[0])
     d = circ.copy()
-    assert not GreedyPauliSimp(0.5, 0.5, thread_timeout=0, only_reduce=True).apply(d)
     assert GreedyPauliSimp(0.5, 0.5, thread_timeout=10, trials=5).apply(d)
 
     assert np.allclose(circ.get_unitary(), d.get_unitary())
