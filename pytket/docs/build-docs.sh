712d12efd3083e12712b372700e212f394e231d1
CalMacCQ <93673602+CalMacCQ@users.noreply.github.com>
2024-12-13 15:34:53 +0000

    make shell script work for linux and mac

--- pytket/docs/build-docs.sh
+++ pytket/docs/build-docs.sh
@@ -2,9 +2,16 @@ cp -R pytket-docs-theming/_static .
 cp -R pytket-docs-theming/quantinuum-sphinx .
 cp pytket-docs-theming/conf.py .
 
-sphinx-build -b html . build -D html_title="pytket"
-find build/ -type f -name "*.html" | xargs sed -e 's/pytket._tket/pytket/g' -i ""
-sed -i '' 's/pytket._tket/pytket/g' build/searchindex.js
+# Replace unnecessary _tket for all classes and functions
+# Apple MACOSX and Linux have differing sed replace syntax
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sphinx-build -b html . build -D html_title="pytket"
+    find build/ -type f -name "*.html" | xargs sed -e 's/pytket._tket/pytket/g' -i ""
+    sed -i '' 's/pytket._tket/pytket/g' build/searchindex.js
+else
+    find build/ -type f -name "*.html" | xargs sed -i 's/pytket._tket/pytket/g'
+    sed -i 's/pytket._tket/pytket/g' build/searchindex.js
+fi
 
 # Remove copied files. This ensures reusability.
 rm -r _static 
