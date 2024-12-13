5f7af8d97d81c620071e8b639a694b3a7135e2f8
Callum Macpherson <93673602+CalMacCQ@users.noreply.github.com>
2024-12-13 16:00:22 +0000

    fix: remove unecessary `_tket` perfix in local docs build (#1721)
    * update build script to replace _tket
    
    * delete unused makefile
    
    * uncomment sed
    
    * fix path for searchindex.js sed
    
    * final fix for search index replace
    
    * make shell script work for linux and mac
    
    * uupdate script to align with website build
    
    * minor edits
    
    * fix comment

--- pytket/docs/build-docs.sh
+++ pytket/docs/build-docs.sh
@@ -2,7 +2,23 @@ cp -R pytket-docs-theming/_static .
 cp -R pytket-docs-theming/quantinuum-sphinx .
 cp pytket-docs-theming/conf.py .
 
-sphinx-build -b html . build -D html_title="pytket"
+# Get pytket package version to be used in page title
+PYTKET_VERSION="$(pip show pytket| grep Version | awk '{print $2}')"
+
+PACKAGE="pytket $PYTKET_VERSION"
+
+# Build the docs setting the html title to show the correct pytket version.
+sphinx-build -b html . build -D html_title="$PACKAGE API documentation"
+
+# Replace unnecessary _tket for all classes and functions in the built html
+# Apple MACOSX and Linux have differing sed replace syntax
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    find build/ -type f -name "*.html" | xargs sed -e 's/pytket._tket/pytket/g' -i ""
+    sed -i '' 's/pytket._tket/pytket/g' build/searchindex.js
+else
+    find build/ -type f -name "*.html" | xargs sed -i 's/pytket._tket/pytket/g'
+    sed -i 's/pytket._tket/pytket/g' build/searchindex.js
+fi
 
 # Remove copied files. This ensures reusability.
 rm -r _static 
