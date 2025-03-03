9067ccce6083238fb8234911393d2aa649035dbd
Alec Edgington <54802828+cqc-alec@users.noreply.github.com>
2025-03-03 14:58:46 +0000

    Update libs to boost 1.87.0. (#1798)

--- libs/tktokenswap/test/conanfile.py
+++ libs/tktokenswap/test/conanfile.py
@@ -59,6 +59,6 @@ class test_tktokenswapRecipe(ConanFile):
         cmake.install()
 
     def requirements(self):
-        self.requires("tktokenswap/0.3.9")
+        self.requires("tktokenswap/0.3.10")
         self.requires("tkrng/0.3.3@tket/stable")
         self.requires("catch2/3.8.0")
