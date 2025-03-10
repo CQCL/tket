67bf37ec74cbc5a2f74126a647cb327666651dd8
Alec Edgington <54802828+cqc-alec@users.noreply.github.com>
2025-03-10 10:12:31 +0000

    Fix error in `separate_classical` when quantum ops succeed final measures (#1803)

--- tket/conanfile.py
+++ tket/conanfile.py
@@ -23,7 +23,7 @@ from conan.errors import ConanInvalidConfiguration
 
 class TketConan(ConanFile):
     name = "tket"
-    version = "2.0.5"
+    version = "2.0.6"
     package_type = "library"
     license = "Apache 2"
     homepage = "https://github.com/CQCL/tket"
