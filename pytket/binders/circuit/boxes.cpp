7eaef9e23080578cd9e9040a0e9e931bc6329f0f
Alec Edgington <alec.edgington@quantinuum.com>
2025-04-02 15:42:30 +0100

    WIP

--- pytket/binders/circuit/boxes.cpp
+++ pytket/binders/circuit/boxes.cpp
@@ -20,6 +20,7 @@
 #include <nanobind/stl/map.h>
 #include <nanobind/stl/optional.h>
 #include <nanobind/stl/pair.h>
+#include <nanobind/stl/shared_ptr.h>
 #include <nanobind/stl/string.h>
 #include <nanobind/stl/vector.h>
 
