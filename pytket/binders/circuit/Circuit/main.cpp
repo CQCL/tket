e1388092db97d106d160e01a3c7fa4fd61906055
Alec Edgington <alec.edgington@quantinuum.com>
2025-04-02 15:37:39 +0100

    WIP

--- pytket/binders/circuit/Circuit/main.cpp
+++ pytket/binders/circuit/Circuit/main.cpp
@@ -18,6 +18,7 @@
 #include <nanobind/operators.h>
 #include <nanobind/stl/map.h>
 #include <nanobind/stl/optional.h>
+#include <nanobind/stl/set.h>
 #include <nanobind/stl/string.h>
 
 #include <optional>
