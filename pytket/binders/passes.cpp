e0c8dbbb4db674daa92fa5dfbbee44348de9fc32
Alec Edgington <alec.edgington@quantinuum.com>
2025-04-02 12:31:23 +0100

    WIP

--- pytket/binders/passes.cpp
+++ pytket/binders/passes.cpp
@@ -13,7 +13,11 @@
 // limitations under the License.
 
 #include <nanobind/nanobind.h>
+#include <nanobind/stl/map.h>
 #include <nanobind/stl/optional.h>
+#include <nanobind/stl/pair.h>
+#include <nanobind/stl/shared_ptr.h>
+#include <nanobind/stl/string.h>
 #include <nanobind/stl/unordered_set.h>
 #include <nanobind/stl/vector.h>
 #include <nanobind/trampoline.h>
