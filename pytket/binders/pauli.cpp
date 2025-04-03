6a8e1ed72c1d54210e730898e512ae782e3af801
Alec Edgington <alec.edgington@quantinuum.com>
2025-04-03 12:36:02 +0100

    WIP

--- pytket/binders/pauli.cpp
+++ pytket/binders/pauli.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <nanobind/eigen/dense.h>
+#include <nanobind/eigen/sparse.h>
 #include <nanobind/nanobind.h>
 #include <nanobind/operators.h>
 #include <nanobind/stl/complex.h>
