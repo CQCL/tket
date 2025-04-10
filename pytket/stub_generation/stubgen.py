a9fbc776446b867fe5ae63480553700679625bc8
Alec Edgington <alec.edgington@quantinuum.com>
2025-04-10 12:32:08 +0100

    ArrayLike -> NDArray

--- pytket/stub_generation/stubgen.py
+++ pytket/stub_generation/stubgen.py
@@ -660,7 +660,7 @@ class StubGen:
 
         - "NoneType" -> "None"
 
-        - "ndarray[...]" -> "Annotated[ArrayLike, dict(...)]"
+        - "ndarray[...]" -> "Annotated[NDArray, dict(...)]"
 
         - "collections.abc.X" -> "X"
           (with "from collections.abc import X" added at top)
@@ -674,7 +674,7 @@ class StubGen:
         def process_ndarray(m: Match[str]) -> str:
             s = m.group(2)
 
-            ndarray = self.import_object("numpy.typing", "ArrayLike")
+            ndarray = self.import_object("numpy.typing", "NDArray")
             assert ndarray
             s = re.sub(r"dtype=([\w]*)\b", r"dtype='\g<1>'", s)
             s = s.replace("*", "None")
