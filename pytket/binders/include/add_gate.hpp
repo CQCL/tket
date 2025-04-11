d1916f2e63e7293ba21032633ba2fc38c8c7aa6d
Will Simmons <will.simmons@cambridgequantum.com>
2025-04-11 15:12:36 +0100

    Use variants to merge methods in pybind (#1832)
    * Use variants to merge methods in pybind
    
    * Changelog entry

--- pytket/binders/include/add_gate.hpp
+++ pytket/binders/include/add_gate.hpp
@@ -15,6 +15,7 @@
 #pragma once
 #include <pybind11/pybind11.h>
 
+#include <variant>
 #include <vector>
 
 #include "tket/Circuit/Circuit.hpp"
@@ -22,95 +23,10 @@
 namespace py = pybind11;
 
 namespace tket {
-template <typename ID>
-static Circuit *add_gate_method_sequence(
+
+Circuit *add_gate_method_any(
     Circuit *circ, const Op_ptr &op,
-    const py::tket_custom::SequenceVec<ID> &args_seq,
-    const py::kwargs &kwargs) {
-  std::vector<ID> args = args_seq;
-  return add_gate_method(circ, op, args, kwargs);
-}
+    const std::variant<std::vector<unsigned>, std::vector<UnitID>> &args,
+    const py::kwargs &kwargs);
 
-template <typename ID>
-static Circuit *add_gate_method(
-    Circuit *circ, const Op_ptr &op, const std::vector<ID> &args,
-    const py::kwargs &kwargs) {
-  if (op->get_desc().is_meta()) {
-    throw CircuitInvalidity("Cannot add metaop to a circuit.");
-  }
-  if (op->get_desc().is_barrier()) {
-    throw CircuitInvalidity(
-        "Please use `add_barrier` to add a "
-        "barrier to a circuit.");
-  }
-  static const std::set<std::string> allowed_kwargs = {
-      "opgroup", "condition", "condition_bits", "condition_value"};
-  for (const auto kwarg : kwargs) {
-    const std::string kwargstr = py::cast<std::string>(kwarg.first);
-    if (!allowed_kwargs.contains(kwargstr)) {
-      std::stringstream msg;
-      msg << "Unsupported keyword argument '" << kwargstr << "'";
-      throw CircuitInvalidity(msg.str());
-    }
-  }
-  std::optional<std::string> opgroup;
-  if (kwargs.contains("opgroup")) {
-    opgroup = py::cast<std::string>(kwargs["opgroup"]);
-  }
-  bool condition_given = kwargs.contains("condition");
-  bool condition_bits_given = kwargs.contains("condition_bits");
-  bool condition_value_given = kwargs.contains("condition_value");
-  if (condition_given && condition_bits_given) {
-    throw CircuitInvalidity("Both `condition` and `condition_bits` specified");
-  }
-  if (condition_value_given && !condition_bits_given) {
-    throw CircuitInvalidity(
-        "`condition_value` specified without `condition_bits`");
-  }
-  if (condition_given) {
-    py::module condition = py::module::import("pytket.circuit.add_condition");
-    py::object add_condition = condition.attr("_add_condition");
-    auto conditions =
-        add_condition(circ, kwargs["condition"]).cast<std::pair<Bit, bool>>();
-    unit_vector_t new_args = {conditions.first};
-    unsigned n_new_args = new_args.size();
-    Op_ptr cond =
-        std::make_shared<Conditional>(op, n_new_args, int(conditions.second));
-    op_signature_t sig = op->get_signature();
-
-    for (unsigned i = 0; i < args.size(); ++i) {
-      switch (sig.at(i)) {
-        case EdgeType::Quantum: {
-          new_args.push_back(Qubit(args[i]));
-          break;
-        }
-        case EdgeType::WASM: {
-          new_args.push_back(WasmState(args[i]));
-          break;
-        }
-        case EdgeType::Classical:
-        case EdgeType::Boolean: {
-          new_args.push_back(Bit(args[i]));
-          break;
-        }
-        default: {
-          TKET_ASSERT(!"add_gate_method found invalid edge type in signature");
-        }
-      }
-    }
-    circ->add_op(cond, new_args, opgroup);
-  } else if (condition_bits_given) {
-    std::vector<ID> new_args =
-        py::cast<std::vector<ID>>(kwargs["condition_bits"]);
-    unsigned n_new_args = new_args.size();
-    unsigned value = condition_value_given
-                         ? py::cast<unsigned>(kwargs["condition_value"])
-                         : (1u << n_new_args) - 1;
-    Op_ptr cond = std::make_shared<Conditional>(op, n_new_args, value);
-    new_args.insert(new_args.end(), args.begin(), args.end());
-    circ->add_op(cond, new_args, opgroup);
-  } else
-    circ->add_op(op, args, opgroup);
-  return circ;
-}
 }  // namespace tket
