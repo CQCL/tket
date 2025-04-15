af2dae4a05bf74ec77fea39386b96092ac5fcefd
Alec Edgington <54802828+cqc-alec@users.noreply.github.com>
2025-04-15 16:09:45 +0100

    Remove nix support. (#1854)

--- nix-support/includes-fixup.nix
+++
@@ -1,7 +0,0 @@
-''
-  # fix bogus include paths
-  # trick found here: https://github.com/NixOS/nixpkgs/blob/master/pkgs/development/libraries/crc32c/default.nix
-  for f in $(find $out/lib/cmake -name '*.cmake'); do
-    substituteInPlace "$f" --replace "\''${_IMPORT_PREFIX}/$out/include" "\''${_IMPORT_PREFIX}/include"
-  done
-''
