7f102520e5d6a46a1fa7daf4ede7b1159f5a28c7
Alec Edgington <alec.edgington@quantinuum.com>
2025-04-16 15:10:03 +0100

    Add note and nanobind license information to stubgen.py.

--- pytket/stub_generation/stubgen.py
+++ pytket/stub_generation/stubgen.py
@@ -2,6 +2,43 @@
 # pyright: strict
 
 """
+NOTE: This file is copied from
+https://github.com/wjakob/nanobind/blob/7e3958086241ec579adfb4d9683bba24e2df80bd/src/stubgen.py
+with a few small changes. We should try to upstream these changes so that it's
+unnecessary to have this copy in our source tree. In the meantime, please keep
+modifications, including formatting, to a minimum, so as to keep the diff simple.
+The nanobind project is licensed under the BSD 3-Clause license, reproduced below:
+============
+Copyright (c) 2022 Wenzel Jakob <wenzel.jakob@epfl.ch>, All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors
+   may be used to endorse or promote products derived from this software
+   without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+============
+"""
+
+"""
 stubgen.py: nanobind stub generation tool
 
 This file provides both an API (``nanobind.stubgen.StubGen``) and a command
