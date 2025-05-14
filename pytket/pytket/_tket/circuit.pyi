79f349a9b2c3aaf2b9b50a6e1f2af4a1ae65637e
Will Simmons <will.simmons1465@gmail.com>
2025-05-14 16:36:12 +0100

    Attempt to format like the other Circuit python methods

--- pytket/pytket/_tket/circuit.pyi
+++ pytket/pytket/_tket/circuit.pyi
@@ -2345,16 +2345,25 @@ class Circuit:
         """
         Append a :py:class:`~.ClExprOp` defined in terms of a logical expression.
 
+
         Example:
+
         >>> c = Circuit()
+
         >>> x_reg = c.add_c_register('x', 3)
+
         >>> y_reg = c.add_c_register('y', 3)
+
         >>> z_reg = c.add_c_register('z', 3)
+
         >>> c.add_clexpr_from_logicexp(x_reg | y_reg, z_reg.to_list())
+
         [ClExpr x[0], x[1], x[2], y[0], y[1], y[2], z[0], z[1], z[2]; ]
 
         :param exp: logical expression
+
         :param output_bits: list of bits in output
+
         :return: the updated circuit
         """
 
--- pytket/pytket/circuit/__init__.py
+++ pytket/pytket/circuit/__init__.py
@@ -164,19 +164,16 @@ def add_clexpr_from_logicexp(
     circ: Circuit, exp: LogicExp, output_bits: list[Bit], **kwargs: Any
 ) -> Circuit:
     """Append a :py:class:`~.ClExprOp` defined in terms of a logical expression.
-
-    Example:
-    >>> c = Circuit()
-    >>> x_reg = c.add_c_register('x', 3)
-    >>> y_reg = c.add_c_register('y', 3)
-    >>> z_reg = c.add_c_register('z', 3)
-    >>> c.add_clexpr_from_logicexp(x_reg | y_reg, z_reg.to_list())
-    [ClExpr x[0], x[1], x[2], y[0], y[1], y[2], z[0], z[1], z[2]; ]
-
-    :param exp: logical expression
-    :param output_bits: list of bits in output
-    :return: the updated circuit
-    """
+    \n\nExample:
+    \n>>> c = Circuit()
+    \n>>> x_reg = c.add_c_register('x', 3)
+    \n>>> y_reg = c.add_c_register('y', 3)
+    \n>>> z_reg = c.add_c_register('z', 3)
+    \n>>> c.add_clexpr_from_logicexp(x_reg | y_reg, z_reg.to_list())
+    \n[ClExpr x[0], x[1], x[2], y[0], y[1], y[2], z[0], z[1], z[2]; ]
+    \n:param exp: logical expression
+    \n:param output_bits: list of bits in output
+    \n:return: the updated circuit"""
     wexpr, args = wired_clexpr_from_logic_exp(exp, output_bits)
     circ.add_clexpr(wexpr, args, **kwargs)
     return circ
