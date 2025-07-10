b313df66fc29d878f86d92e2cba0cbdb1caee327
«Deploy tket C++ documentation» github action <tket-bot@cambridgequantum.com>
2025-07-10 15:35:53 +0000

    Add generated tket C++ documentation.

--- docs/tket/api/search/typedefs_4.js
+++ docs/tket/api/search/typedefs_4.js
@@ -3,7 +3,7 @@ var searchData=
   ['e_5fin_5fiterator_0',['E_in_iterator',['../namespacetket.html#aba87c4fb667f355f1b037432e733b2b8',1,'tket']]],
   ['e_5fiterator_1',['E_iterator',['../namespacetket.html#a87b3e8d61fc90082021f300979610324',1,'tket']]],
   ['e_5fout_5fiterator_2',['E_out_iterator',['../namespacetket.html#abeeae412afb5d0ffb412ae94d913d52c',1,'tket']]],
-  ['edge_3',['edge',['../namespacetket.html#a6a99d9f2b64decd8803090cc890e1e7d',1,'tket::Edge'],['../namespacetket_1_1graphs_1_1utils.html#a0ddb76f99e73585abb2f33d56bd69710',1,'tket::graphs::utils::edge'],['../namespacetket_1_1graphs_1_1utils_1_1detail.html#a42016975da7fc120be70805a88880f55',1,'tket::graphs::utils::detail::edge'],['../classtket_1_1graphs_1_1AbstractGraph.html#a505720476bea924e4685ecaef2d73f73',1,'tket::graphs::AbstractGraph::Edge']]],
+  ['edge_3',['edge',['../namespacetket_1_1graphs_1_1utils.html#a0ddb76f99e73585abb2f33d56bd69710',1,'tket::graphs::utils::edge'],['../namespacetket_1_1graphs_1_1utils_1_1detail.html#a42016975da7fc120be70805a88880f55',1,'tket::graphs::utils::detail::edge'],['../namespacetket.html#a6a99d9f2b64decd8803090cc890e1e7d',1,'tket::Edge'],['../classtket_1_1graphs_1_1AbstractGraph.html#a505720476bea924e4685ecaef2d73f73',1,'tket::graphs::AbstractGraph::Edge']]],
   ['edge_5fmap_5ft_4',['edge_map_t',['../namespacetket.html#a526342a687fb4be46f3fe470c52aebf4',1,'tket']]],
   ['edge_5fpair_5ft_5',['edge_pair_t',['../namespacetket.html#af2657be7b0d809b5ca3814d03365598c',1,'tket']]],
   ['edgelist_6',['EdgeList',['../namespacetket.html#a3f1255a37e552199066907e589647e7d',1,'tket']]],
