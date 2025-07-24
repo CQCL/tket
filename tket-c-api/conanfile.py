eeb1a56f255f5a92f1ee8dd46c79fae1778a74e2
Luca Mondada <luca@mondada.net>
2025-07-24 12:58:18 +0200

    address comments

--- tket-c-api/conanfile.py
+++ tket-c-api/conanfile.py
@@ -1,7 +1,8 @@
+import os
+
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout, CMakeDeps
 from conan.tools.files import copy, load
-import os
 
 
 class TketCAPIRecipe(ConanFile):
