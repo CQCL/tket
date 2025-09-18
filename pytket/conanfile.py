3547e5525abecde0581bbd1f2f9a6d6383477ec9
Alec Edgington <alec.edgington@quantinuum.com>
2025-09-18 09:40:35 +0100

    Bump versions of boost, tktokenswap, tkwsm and symengine.

--- pytket/conanfile.py
+++ pytket/conanfile.py
@@ -50,16 +50,16 @@ class pytketRecipe(ConanFile):
     def requirements(self):
         tket_version = self._get_tket_version()
 
-        self.requires("gmp/6.3.0")
+        self.requires("gmp/tci-6.3.0@tket/stable")
         self.requires("nlohmann_json/3.12.0")
         self.requires("nanobind/tci-2.9.2@tket/stable")
-        self.requires("symengine/tci-0.14.0.1@tket/stable")
+        self.requires("symengine/tci-0.14.0.2@tket/stable")
         self.requires("tkassert/0.3.4@tket/stable")
         self.requires(f"tket/{tket_version}@tket/stable")
         self.requires("tklog/0.3.3@tket/stable")
         self.requires("tkrng/0.3.3@tket/stable")
-        self.requires("tktokenswap/0.3.11@tket/stable")
-        self.requires("tkwsm/0.3.11@tket/stable")
+        self.requires("tktokenswap/0.3.13@tket/stable")
+        self.requires("tkwsm/0.3.13@tket/stable")
 
     def export(self):
         # Copy the TKET_VERSION file to the export folder
