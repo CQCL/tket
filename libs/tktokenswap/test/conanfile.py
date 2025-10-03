8e42b1acb1b76ac74563983c84ac7e5a38a7c4af
Alec Edgington <alec.edgington@quantinuum.com>
2025-10-03 11:11:52 +0100

    Use catch2 3.11.0.

--- libs/tktokenswap/test/conanfile.py
+++ libs/tktokenswap/test/conanfile.py
@@ -61,4 +61,4 @@ class test_tktokenswapRecipe(ConanFile):
     def requirements(self):
         self.requires("tktokenswap/0.3.13")
         self.requires("tkrng/0.3.3@tket/stable")
-        self.requires("catch2/3.10.0@tket/stable")
+        self.requires("catch2/3.11.0@tket/stable")
